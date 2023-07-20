d194f1331dfbc76f2eea8ff630f580150a81dbba
Seung Hyun Lee <92029126+seunghyun980@users.noreply.github.com>
2023-07-20 16:40:51 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 416 ms, Memory: 19836 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int[][] a = new int[n][m];
+		int[][] b = new int[n][m];
+		
+		for(int i=0; i<n; i++) {
+			st = new StringTokenizer(bf.readLine());
+			for(int j=0; j<m; j++) {
+				a[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		for(int i=0; i<n; i++) {
+			st = new StringTokenizer(bf.readLine());
+			for(int j=0; j<m; j++) {
+				b[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<m; j++) {
+				System.out.print(a[i][j] + b[i][j] + " ");
+			}
+			System.out.println("");
+		}
+	}
+
+}
+
+
