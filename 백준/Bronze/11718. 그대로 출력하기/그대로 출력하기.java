386c10716c996772235f144f745d07ccc65785b3
Seung Hyun Lee <92029126+seunghyun980@users.noreply.github.com>
2023-07-24 17:22:53 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 128 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,18 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+    	BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    	
+    	String result = "";
+    	
+    	while((result = bf.readLine()) != null) {
+    			
+    		System.out.println(result);
+    		
+    	}
+    }
+}
