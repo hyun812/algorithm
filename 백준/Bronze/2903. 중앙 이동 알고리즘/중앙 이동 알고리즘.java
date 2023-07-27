135969853c5fac33df5d81a0cb5c079beb135902
Seung Hyun Lee <92029126+seunghyun980@users.noreply.github.com>
2023-07-27 23:29:28 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 124 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+
+		int du = 2;
+		int add = 1;
+		for (int i = 0; i < n; i++) {
+			du = du + add;
+			add *= 2;
+
+		}
+
+		System.out.println(du * du);
+
+	}
+}
