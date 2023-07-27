ff46935d31761ad114b9f169d0e941925c9e56fc
Seung Hyun Lee <92029126+seunghyun980@users.noreply.github.com>
2023-07-27 23:43:38 +0900

    [Bronze III] Title: 배수와 약수, Time: 120 ms, Memory: 13976 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.java
@@ -0,0 +1,28 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		while (true) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			if (a == 0 && b == 0)
+				break;
+
+			if (b % a == 0) {
+				System.out.println("factor");
+			} else if (a % b == 0) {
+				System.out.println("multiple");
+			} else {
+				System.out.println("neither");
+			}
+		}
+	}
+}
