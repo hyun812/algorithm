06355b210e8a1e7f442a517cc8747fc2b7af4875
Seung Hyun Lee <92029126+seunghyun980@users.noreply.github.com>
2023-08-01 09:17:38 +0900

    [Bronze II] Title: 분해합, Time: 220 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        int result=0;
+
+        for(int i=0; i<n; i++){
+            int sum=0;
+            int number=i;
+
+            while(number !=0){
+                sum += number%10;
+                number /= 10;
+            }
+
+            if(sum+i == n){
+                result = i;
+                break;
+            }
+        }
+
+        // for(int i=0; i<n; i++){
+        //     result = i + i/1000000 + (i%1000000)/100000 + (i%100000)/10000 + (i%10000)/1000 + (i%1000)/100 + (i%100)/10 + (i%10);
+
+        //     if(result == n){
+        //         System.out.println(i);
+        //         return;
+        //     }
+        // }
+
+        System.out.println(result);
+        return;
+        
+    }
+}
