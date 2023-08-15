1e458a7a96b6bf7b22304c87aa811887a78dcd39
SeungHyun <92029126+hyun812@users.noreply.github.com>
2023-08-15 22:40:23 +0900

    [Silver II] Title: 좌표 압축, Time: 2192 ms, Memory: 267340 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -9,35 +9,29 @@ public class Main {
         StringTokenizer st;
 
         int n = Integer.parseInt(bf.readLine());
-        HashSet<Integer> hs = new HashSet<>();
 
         HashMap<Integer,Integer> hm = new HashMap<>();
-        Integer[] arr = new Integer[n];
+        int[] origin = new int[n];
+        int[] sorted = new int[n];
 
         st = new StringTokenizer(bf.readLine());
         for(int i=0; i<n; i++){
             int a = Integer.parseInt(st.nextToken());
-            hs.add(a);
-            arr[i] = a;
+            origin[i] = sorted[i] = a;
         }
         
-        Integer[] clone = arr.clone();
-        Arrays.sort(clone, (o1, o2)->{
-            return o2-o1;
-        });
-        
-        int cnt = hs.size()-1;
-        hm.put(clone[0], cnt);
+        Arrays.sort(sorted);
+        int cnt = 0;
 
-        for(int i=1; i<clone.length; i++){
-            if(clone[i].equals(clone[i-1])){
-                continue;
+        for(int value : sorted){
+            if(!hm.containsKey(value)){
+                hm.put(value, cnt);
+                cnt++;
             }
-            hm.put(clone[i], --cnt);
         }
 
-        for(int i=0; i<arr.length; i++){
-            sb.append(hm.get(arr[i])).append(" ");
+        for(int i=0; i<origin.length; i++){
+            sb.append(hm.get(origin[i])).append(" ");
         }
 
         System.out.println(sb.toString());
