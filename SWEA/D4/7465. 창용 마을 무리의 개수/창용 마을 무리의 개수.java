b5f8cd4913cb04785923e5aff4ee2983d69d33d4
SeungHyun <92029126+hyun812@users.noreply.github.com>
2023-08-23 10:40:18 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 124 ms, Memory: 24,532 KB -BaekjoonHub

--- SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
@@ -1,59 +1,71 @@
-import java.util.*;
-import java.io.*;
- 
- 
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
 public class Solution {
-    static int[] parent;
-    public static void main(String[] args) throws Exception {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine());
- 
-        int T = Integer.parseInt(st.nextToken());
- 
-        for(int tc=1; tc<=T; tc++) {
-            st = new StringTokenizer(bf.readLine());
-            int n = Integer.parseInt(st.nextToken());
-            int m = Integer.parseInt(st.nextToken());
- 
-            parent = new int[n+1];
-            for(int i=1; i<=n; i++) {
-                parent[i] = i;
-            }
- 
-            for(int i=0; i<m; i++) {
-                st = new StringTokenizer(bf.readLine());
- 
-                int a = Integer.parseInt(st.nextToken());
-                int b = Integer.parseInt(st.nextToken());
- 
- 
-                union(a,b);
-            }
-             
-            int result = 0;
-            for(int i=1; i<=n; i++) {
-                if(parent[i] == i) {
-                    result++;
-                }
-            }
-            System.out.println("#"+tc+" "+result);
- 
-        }
- 
-    }
- 
-    public static void union(int x, int y) {
-        x = find(x);
-        y = find(y);
- 
-        if(x>y) parent[y]=  x;
-        if(x<y) parent[x] = y;
-    }
- 
- 
-    public static int find(int x) {
-        if(parent[x] == x) return x;
-        return find(parent[x]);
-    }
-    //  parent[x] = 
+
+	static int[] parents;
+	static int n;
+	public static void main(String[] args) throws Exception {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;		
+		
+		int t = Integer.parseInt(bf.readLine());
+		
+		for(int tc=1; tc<=t; tc++) {
+			st = new StringTokenizer(bf.readLine());	
+			
+			n = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
+			int ans =0;
+			make();
+			
+			for(int i=0; i<m; i++) {
+				st = new StringTokenizer(bf.readLine());
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				
+				union(a,b);
+				
+			}
+			
+			for(int i=1; i<=n; i++) {
+				if(parents[i]==i) ans++;
+			}
+			sb.append("#").append(tc).append(" ").append(ans).append("\n");
+		}
+		System.out.println(sb.toString());
+	}
+	
+	private static void make() {
+		parents = new int[n+1];
+		
+		for(int i=0; i<=n; i++) {
+			parents[i] = i;
+		}
+	}
+	
+	private static int find(int a) {
+		if(a == parents[a]) return a;
+		else {
+			int b = find(parents[a]);
+			parents[a] = b;
+			return b;
+		}
+	}
+	
+	private static boolean union(int a, int b) {
+		int aRoot = find(a);
+		int bRoot = find(b);
+		
+		if(aRoot == bRoot) return false;
+		else {
+			parents[bRoot] = aRoot;
+			return true;
+		}
+	}
 }
