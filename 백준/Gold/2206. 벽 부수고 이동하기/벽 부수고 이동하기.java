64f2ecc60f6130a899ab9cb5458a2b677fa19427
SeungHyun <92029126+hyun812@users.noreply.github.com>
2023-09-03 19:58:42 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 768 ms, Memory: 147220 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -1,77 +1,95 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main{
-    static class Node{
-	int x;
-	int y;
-	int dist;
-	int boom;
-	public Node(int x, int y, int dist, int boom){
-		this.x = x;
-		this.y = y;
-		this.dist = dist;
-		this.boom = boom;
-	}
-}
-	static final int dx[] = {0,0,1,-1};
-	static final int dy[] = {1,-1,0,0};
-	static int n,m,ans;
-	static boolean visit[][][];
-	static int map[][];
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		map = new int[n][m];
-		visit = new boolean[n][m][2];
-		ans = -1;
+public class Main {
+    static int n, m;
+    static int[][] arr;
+    static boolean[][][] visited;
 
-		for(int i=0; i<n; i++) {
-			String s = br.readLine();
-			for(int j=0; j<m; j++) {
-				map[i][j] = s.charAt(j) - '0';
-			}
-		}
-		bfs();
-		
-		System.out.println(ans);
-			
-	}
-	
-	static void bfs() {
-		Queue<Node> q = new LinkedList<>();
-		q.add(new Node(0,0,1,0));
-		visit[0][0][0] = true;
-		
-		while(!q.isEmpty()) {
-			Node now = q.poll();
-			int x = now.x;
-			int y = now.y;
-			
-			if(x==n-1 && y == m-1) {
-				ans = now.dist;
-				return;
-			}
-			
-			for(int k=0; k<4; k++) {
-				int nx = x + dx[k];
-				int ny = y + dy[k];
-				if(0>nx || nx>=n || 0>ny || ny>=m) continue;
-				
-				if(map[nx][ny] == 0 && !visit[nx][ny][now.boom]) { //벽이 아닐경우
-					visit[nx][ny][now.boom] = true;
-					q.add(new Node(nx,ny,now.dist+1,now.boom));
-				}else { //벽이 있는경우
-					if(now.boom == 0 && !visit[nx][ny][now.boom+1]) { //벽을 부순적이 없을때
-						visit[nx][ny][now.boom+1] = true;
-						q.add(new Node(nx,ny,now.dist+1,now.boom+1));
-					}
-				}
-				
-			}
-		}
-		
-	}
+    static class Loc{
+        int y,x, cnt, destroyed;
+
+        public Loc(int y, int x, int cnt, int destroyed) {
+            this.y = y;
+            this.x = x;
+            this.cnt = cnt;
+            this.destroyed = destroyed;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n][m];
+        visited = new boolean[n][m][2];
+        // visited[n][m][0] 은 벽을 한번도 안부순 애들의 탐색
+        // visited[n][m][1] 은 벽을 한번 부순 애들의 탐색
+
+        for(int i=0; i<n; i++) {
+            String s = bf.readLine();
+            for(int j=0; j<m; j++) {
+                arr[i][j] = s.charAt(j) - '0';
+            }
+        }
+
+        bfs();
+
+    }
+
+    static int[] dy = {-1, 1, 0, 0};
+    static int[] dx = {0, 0, -1, 1};
+
+    private static void bfs() {
+        Queue<Loc> q = new LinkedList<>();
+        q.add(new Loc(0,0,1,0));
+        visited[0][0][0] = true;
+
+        while(!q.isEmpty()) {
+            Loc now = q.poll();
+            // int y = now.y;
+            // int x = now.x;
+
+            if(now.y == n-1 && now.x == m-1){
+                System.out.println(now.cnt);
+                return;
+            }
+
+            for(int i=0; i<4; i++) {
+                int ny = now.y + dy[i];
+                int nx = now.x + dx[i];
+
+                if(!outOfIdx(ny, nx)) continue;
+
+                int next_cnt = now.cnt+1;
+
+                if(arr[ny][nx] == 0) { // 벽이 아니면
+                    if(!visited[ny][nx][now.destroyed]){ // 부신 벽이 여태까지 없었으면
+                        visited[ny][nx][now.destroyed] = true;
+                        q.add(new Loc(ny,nx,next_cnt, now.destroyed));
+
+                    }
+                } else if (arr[ny][nx] == 1) { // 벽이면
+                    if(now.destroyed == 0 && !visited[ny][nx][now.destroyed+1]){ // 한번도 부순적이 없다면
+                        visited[ny][nx][now.destroyed+1] = true;
+                        q.add(new Loc(ny,nx,next_cnt,now.destroyed+1));
+
+                    }
+                    // 한번 부순적이 있다면 또 부수고 갈수 없기 때문에 pass
+                }
+            }
+        }
+        System.out.println(-1);
+    }
+
+    private static boolean outOfIdx(int y, int x) {
+        if(y>=0 && y<n && x>=0 && x<m) {
+            return true;
+        }
+        return false;
+    }
 }
+
