8bc9cdc609eb011cf0fb6115f1eab52ebd47cd5f
SeungHyun <92029126+hyun812@users.noreply.github.com>
2023-09-24 17:22:39 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보호 필름, Time: 354 ms, Memory: 23,276 KB -BaekjoonHub

--- SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.java
+++ SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.java
@@ -1,93 +1,97 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Solution {
-    static int d, w, k;
-    static int[][] arr, copyArr;
-    static int ans;
+
+    static int res;
+    static int d,w,k;  // 두께 D, 가로크기 W, 합격기준 K
+    static int[][] map;
+    static int[] arr;
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
+        int tc = Integer.parseInt(bf.readLine());
 
-        int t = Integer.parseInt(bf.readLine());
-        for(int tc=1; tc<=t; tc++) {
+        for(int t =  1; t <= tc; t++) {
             st = new StringTokenizer(bf.readLine());
             d = Integer.parseInt(st.nextToken());
             w = Integer.parseInt(st.nextToken());
             k = Integer.parseInt(st.nextToken());
-            arr = new int[d][w];
-            copyArr = new int[d][w];
+
+            map = new int[d][w];
+            res = d;
 
             for(int i=0; i<d; i++){
                 st = new StringTokenizer(bf.readLine());
                 for(int j=0; j<w; j++){
-                    int a = Integer.parseInt(st.nextToken());
-                    arr[i][j] = a;
-                    copyArr[i][j] = a;
+                    map[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
 
-            for(int i=0; i<d; i++){
-                if(doIt(0, 0, i)){
-                    ans = i;
+            arr = new int[d];
+            Arrays.fill(arr, -1); // -1로 채우기
+
+            dfs(0,0);
+            System.out.println("#"+ t + " " + res);
+        }
+
+    }
+    static void dfs(int idx, int cnt) {
+        if(cnt >= res){
+            return;
+        }
+
+        if(idx == d){
+            if(check()){
+                res = Math.min(res,cnt);
+            }
+            return;
+        }
+
+        arr[idx] = -1;
+        dfs(idx+1, cnt);
+
+        arr[idx] = 0;
+        dfs(idx+1, cnt+1);
+
+        arr[idx] = 1;
+        dfs(idx+1, cnt+1);
+    }
+
+    private static boolean check() { // 성능 검사
+        int cnt;
+        int a, b;
+        for(int i=0; i<w; i++){
+            cnt = 1;
+            for(int j=0; j<d-1; j++){
+                a = map[j][i];
+                b = map[j+1][i];
+
+                if(arr[j] != -1){
+                    a = arr[j];
+                }
+                if(arr[j+1] != -1){
+                    b = arr[j+1];
+                }
+
+                if(a == b){
+                    cnt++;
+                }else{
+                    cnt = 1;
+                }
+
+                if(cnt == k){
                     break;
                 }
-                copy();
+
+
             }
-            System.out.println("#"+tc+" "+ans);
-        }
-    }
-
-    private static boolean doIt(int idx, int k, int drugCnt){
-        if(k == drugCnt){
-            return checkArr(drugCnt);
-        }
-
-        for(int i=idx; i<d; i++){ // 몇번째 줄에 약을 투입할지
-            for(int j=0; j<2; j++){ // 어떤 타입에 약을 투입할지
-                addType(i,j);
-                if(doIt(i+1, k+1, drugCnt)) return true;
-                delType(i);
-            }
-        }
-        return false;
-    }
-
-    private static void addType(int x, int type){
-        for(int i=0; i<w; i++){
-            copyArr[x][i] = type;
-        }
-    }
-
-    private static void delType(int x){
-        for(int i=0; i<w; i++){
-            copyArr[x][i] = arr[x][i];
-        }
-    }
-
-    private static boolean checkArr(int a) {
-        for (int j = 0; j < w; j++) {
-            int cnt = 1;
-            boolean flag = false;
-            for (int i = 0; i < d-1; i++) {
-                if(copyArr[i][j]==copyArr[i+1][j]) cnt++;
-                else cnt = 1;
-
-                if(cnt == k) {
-                    flag = true;
-                    break;
-                }
-            }
-            if(!flag) return false;
+            if(cnt < k) return false;
         }
         return true;
-    }
-
-    private static void copy(){
-        for(int i=0; i<d; i++){
-            for(int j=0; j<w; j++){
-                copyArr[i][j] = arr[i][j];
-            }
-        }
     }
 }
