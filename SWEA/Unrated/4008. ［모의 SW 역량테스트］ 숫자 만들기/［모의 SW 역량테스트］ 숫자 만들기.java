5033de012bc704846a708362b2efdc7d89879f91
SeungHyun <92029126+hyun812@users.noreply.github.com>
2023-09-27 10:36:20 +0900

    [Unrated] Title: [모의 SW 역량테스트] 숫자 만들기, Time: 111 ms, Memory: 20,956 KB -BaekjoonHub

--- SWEA/Unrated/4008. ［모의 SW 역량테스트］ 숫자 만들기/［모의 SW 역량테스트］ 숫자 만들기.java
+++ SWEA/Unrated/4008. ［모의 SW 역량테스트］ 숫자 만들기/［모의 SW 역량테스트］ 숫자 만들기.java
@@ -1,89 +1,78 @@
-
-
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Solution {
-	static StringBuilder sb = new StringBuilder();
-	static int[] num;
-	static int[] ope;
-	static int[] make;
+
 	static int n;
+	static int[] nums;
+	static int[] ope;
 	static int max;
 	static int min;
+
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(bf.readLine());
+		StringTokenizer st;
 		
-		int t = Integer.parseInt(st.nextToken());
-		
+		int t = Integer.parseInt(bf.readLine());
 		for(int tc = 1; tc<=t; tc++) {
+			
+			n = Integer.parseInt(bf.readLine());
+			
 			max = Integer.MIN_VALUE;
 			min = Integer.MAX_VALUE;
-			
-			st = new StringTokenizer(bf.readLine());
-			n = Integer.parseInt(st.nextToken());
-			
 			ope = new int[4];
-			num = new int[n];
-			make = new int[n-1];
-		
+			nums = new int[n];
+			
 			st = new StringTokenizer(bf.readLine());
 			for(int i=0; i<4; i++) {
-				ope[i] =Integer.parseInt(st.nextToken()); 
+				ope[i] = Integer.parseInt(st.nextToken());
 			}
 			
 			st = new StringTokenizer(bf.readLine());
 			for(int i=0; i<n; i++) {
-				num[i] = Integer.parseInt(st.nextToken());
+				nums[i] = Integer.parseInt(st.nextToken());
 			}
 			
-			
-			check(0);
-			
-		 	sb.append("#").append(tc).append(" ").append(max-min).append("\n");
-			
+			doit(0, nums[0]);
+			System.out.println("#"+tc+" " + (max-min));
 		}
-		System.out.println(sb.toString());
 	}
 	
-	public static void check(int cnt) {
-		if(n-1 == cnt) {
-			minmax();
+	private static void doit(int cnt, int num) {
+		if(cnt == n-1) {
+			max = Math.max(max, num);
+			min = Math.min(min, num);
+			
 			return;
 		}
 		
 		for(int i=0; i<4; i++) {
-			if(ope[i] == 0) continue;
-			ope[i]--;
-			make[cnt] = i;
-			check(cnt+1);
-			ope[i]++;
+			if(ope[i] > 0) {
+				ope[i]--;
+				doit(cnt+1, calc(num, cnt, i));
+				ope[i]++;	
+			}
 		}
 	}
 	
-	public static void minmax() {
-		int ans = num[0];
-		for(int i=0; i<n-1; i++) {
-			// +
-			if(make[i] == 0) {
-				ans+=num[i+1];
-			}
-			// -
-			else if(make[i] == 1) {
-				ans-=num[i+1];
-			}
-			// *
-			else if(make[i] == 2) {
-				ans*=num[i+1];
-			}
-			// /
-			else if(make[i] == 3) {
-				ans/=num[i+1];
-			}
+	private static int calc(int num, int cnt, int idx) { // num은 기존 계산한 값 , cnt는 숫자의 인덱스, idx는 사용할 연산자의 인덱스
+		int res = num;
+		switch(idx) {
+		case 0:
+			res+=nums[cnt+1];
+			break;
+		case 1:
+			res-=nums[cnt+1];
+			break;
+		case 2:
+			res*=nums[cnt+1];
+			break;
+		case 3:
+			res/=nums[cnt+1];
+			break;
 		}
-		max = Math.max(max, ans);
-		min = Math.min(min, ans);
+		
+		return res;
 	}
 }
