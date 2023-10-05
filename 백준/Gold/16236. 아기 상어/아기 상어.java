53074121963cb7b0826a27282782aa0aadf8c353
SeungHyun <92029126+hyun812@users.noreply.github.com>
2023-10-05 21:38:41 +0900

    [Gold III] Title: 아기 상어, Time: 88 ms, Memory: 12100 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.java
+++ 백준/Gold/16236. 아기 상어/아기 상어.java
@@ -2,130 +2,131 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int n;
-    static int[][] arr;
-    static int startY, startX;
-    static PriorityQueue<fish> fishq;
-    static int ans;
-    static int sharkSize;
-    static boolean[][] visited;
-    static int cnt; // 물고기 먹을때마다 카운트
-    public static void main(String[] args) throws Exception {
+	static int n;
+	static int[][] arr;
+	static boolean[][] visited;
+	static int startY, startX;
+	static int ans;
+	static int Bsize, Bcnt; // 아기상어의 크기와 그 크기 계산을 위한 cnt변수
+	static Queue<fish> q;
+	// 처음 아기상어의 크기는 2
+	// 자신의 크기보다 작은 물고기만 먹을 수 있음
+	// 자신과 크기가 같은 물고기는 먹을 순없지만 지나갈 수는 있음
+	// 먹을 수 있는 물고기가 1마리보다 많으면 거리가 가까운순
+	// 거리가 가까운 물고기가 여러개면 가장위에 있는 물고기 , 그것도 여러개면 가장 왼쪽
+	// 아기상어는 자신의 크기와 같은 수의 물고기를 먹을 때 마다 크기가 1증가
+	// 몇초동안 엄마상어에게 도움을 요청하지 않고 물고기를 잡아먹을 수 있는지 구하라!!
+	public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-
-        // 크기가 큰 물고기가 있는 칸은 못지나감
-        // 크기가 같은 물고기가 있는 칸은 지나갈 수 는 있지만 먹지는 못함
-        // 크기가 작은 물고기가 있는 칸은 먹을 수 있음
-
-        // 거리가 가까운 물고기가 많다면 가장 위에 있는 물고기 y좌표
-        // 여러마리라면 가장 왼쪽에 있는 물고기 x좌표
-
-        // 아기 상어는 자신의 크기와 같은 수의 물고기를 먹을 때 마다 크기 1증가
-        // 크기가 2인 상어가 물고기 2마리 먹으면 3됨
-
-        n = Integer.parseInt(bf.readLine());
+        
+        n = Integer.parseInt(bf.readLine().trim()); // 크기
         arr = new int[n][n];
-        visited = new boolean[n][n];
-        sharkSize = 2;
-
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(bf.readLine());
-            for (int j = 0; j < n; j++) {
-                int in = Integer.parseInt(st.nextToken());
-                arr[i][j] = in;
-
-                if (in == 9) {
-                    startY = i;
-                    startX = j;
-                    arr[i][j] = 0;
-                }
-            }
+        Bsize = 2;
+        Bcnt = 0;
+        startY = startX = 0;
+        q = new PriorityQueue<>();
+        
+        for(int i=0; i<n; i++) {
+        	st = new StringTokenizer(bf.readLine());
+        	for(int j=0; j<n; j++) {
+        		arr[i][j] = Integer.parseInt(st.nextToken());
+        		if(arr[i][j] == 9) {
+            		startY = i;
+        			startX = j;
+        			arr[i][j] = 0;
+        		}
+        	}
         }
-        fishq = new PriorityQueue<>( ((o1, o2) -> {
-            if(o1.distance == o2.distance){
-                if(o1.y == o2.y){
-                    return o1.x-o2.x;
-                } return o1.y-o2.y;
-            } return o1.distance - o2.distance;
-        }));
-
-
+        
         bfs();
-
         System.out.println(ans);
-    }
+	}
+	static int[] dy = {-1, 1, 0, 0};
+	static int[] dx = {0, 0, -1, 1};
+	
+	private static void bfs() {
+		visited = new boolean[n][n];
+		q.add(new fish(startY, startX, 0));
+		visited[startY][startX] = true;
+		
+        while(!q.isEmpty()) {
+        	fish f = q.poll();
+        	int y = f.y;
+        	int x = f.x;
+        	
+        	for(int i=0; i<4; i++) {
+        		int ny = y + dy[i];
+        		int nx = x + dx[i];
+        		
+        		if(!outOfIdx(ny, nx)) continue; // 범위를 벗어났으면
+        		if(visited[ny][nx]) continue; // 방문했던 적이 있으면
+        		if(Bsize < arr[ny][nx]) continue; // 아기 상어의 사이즈 보다 크면
 
-    static int[] dy = {-1, 1, 0, 0};
-    static int[] dx = {0, 0, -1, 1};
-
-    private static void bfs() {
-        //초기세팅
-
-        fishq.add(new fish(startY,startX,0));
-        visited[startY][startX] = true;
-
-
-        while (!fishq.isEmpty()) {
-            fish f = fishq.poll();
-            int y = f.y;
-            int x = f.x;
-            int d = f.distance;
-
-            for (int i = 0; i < 4; i++) {
-                int ny = y + dy[i];
-                int nx = x + dx[i];
-
-                if (!outOfIdx(ny, nx)) continue; // 범위 벗어나면
-                if (visited[ny][nx]) continue; // 방문했으면
-
-                visited[ny][nx] = true;
-                if (arr[ny][nx] <= sharkSize) {
-                    fishq.add(new fish(ny, nx, d + 1));
-                }
-            }
-
-            if (fishq.peek() != null) {
-                fish peek = fishq.peek();
-                int y2 = peek.y;
-                int x2 = peek.x;
-                int d2 = peek.distance;
-
-                if (arr[y2][x2] != 0 && arr[y2][x2] < sharkSize ) { // 먹을 수 있는 물고기면
-                    cnt++;
-                    if (cnt == sharkSize) {
-                        sharkSize++;
-                        cnt = 0;
-                    }
-                    arr[y2][x2] = 0;
-
-                    fishq.clear();
-                    fishq.add(new fish(y2, x2, 0));
-                    ans += d2;
-                    visited = new boolean[n][n];
-                    visited[y2][x2] = true;
-                }
-            }
-
+        		visited[ny][nx] = true;
+        		q.add(new fish(ny, nx, f.dis+1));
+        	}
+        	
+        	if(!q.isEmpty()) {
+        		fish fp = q.peek();
+        		int y1 = fp.y;
+        		int x1 = fp.x;
+        		
+        		// 먹을 수 있는 물고기 이면
+        		if(arr[y1][x1] != 0 && Bsize > arr[y1][x1]) { // 0이아니고 아기상어 사이즈보다 작으면
+        			Bcnt++;
+        			if(Bcnt == Bsize) {
+        				Bcnt =0;
+        				Bsize++;
+        			}
+        			arr[y1][x1] = 0;
+        			
+        			q.clear();
+        			
+        			q.add(new fish(y1, x1, 0));
+        			visited = new boolean[n][n];
+        			visited[y1][x1] = true;
+        			
+        			ans += fp.dis;
+        		}
+        		
+        	}
         }
-    }
+	}
+	
+	private static boolean outOfIdx(int ny, int nx) {
+		if(ny>=0 && ny<n && nx>=0 && nx<n) {
+			return true;
+		}
+		return false;
+	}
+	
+	static class fish implements Comparable<fish>{
+		int y, x, dis;
 
-    private static boolean outOfIdx(int y, int x){
-        if(y>=0 && y<n && x>=0 && x<n){
-            return true;
-        }
-        return false;
-    }
-    static class fish{
-        int y, x,distance;
+		public fish(int y, int x, int dis) {
+			super();
+			this.y = y;
+			this.x = x;
+			this.dis = dis;
+		}
 
-        public fish(int y, int x, int distance) {
-            this.y = y;
-            this.x = x;
-            this.distance = distance;
-        }
-    }
+		@Override
+		public String toString() {
+			return "fish [y=" + y + ", x=" + x + ", dis=" + dis + "]";
+		}
 
-
-
+		@Override
+		public int compareTo(fish o) {
+			if(this.dis == o.dis) {
+				if(this.y == o.y) {
+					return this.x - o.x;
+				}else {
+					return this.y - o.y;
+				}
+			}else {
+				return this.dis-o.dis;
+			}
+		} 
+	}
 }
