4375613ffa6a618f210874bd5daf17267e5ed9d3
SeungHyun <92029126+hyun812@users.noreply.github.com>
2023-10-11 01:38:46 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 129 ms, Memory: 25,664 KB -BaekjoonHub

--- SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
@@ -1,66 +1,72 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Solution {
-    static int n, m;
-    static int[] parents;
-    public static void main(String[] args) throws Exception {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
-        int t = Integer.parseInt(bf.readLine());
-
-        for(int tc=1; tc<=t; tc++){
-            st = new StringTokenizer(bf.readLine());
-            n = Integer.parseInt(st.nextToken()); // 정점의 개수
-            m = Integer.parseInt(st.nextToken()); // 간선의 개수
-
-            parents = new int[n+1];
-            make();
-
-            for(int i=0; i<m; i++){
-                st = new StringTokenizer(bf.readLine());
-                int a = Integer.parseInt(st.nextToken());
-                int b = Integer.parseInt(st.nextToken());
-
-                union(a,b);
-            }
-
-            // root찾아서 set에 넣기
-            // 중복제거를 통해 무리가 몇갠지 확인 가능
-            Set<Integer> set = new HashSet<>();
-            for(int j=1; j<=n; j++){
-                int root = find(j);
-                set.add(root);
-            }
-
-            // set에 남아있는 무리의 개수
-            System.out.println("#" + tc + " "+ set.size());
-        }
-    }
-
-    private static void make(){
-        for(int i=1; i<=n; i++){
-            parents[i] = i;
-        }
-    }
-
-    private static boolean union(int a, int b){
-        int aRoot = find(a);
-        int bRoot = find(b);
-
-        if(aRoot == bRoot) return false;
-
-        parents[bRoot] = aRoot;
-        return true;
-    }
-
-    private static int find(int a){
-        if(a == parents[a]) return a;
-        else {
-            int b = find(parents[a]);
-            parents[a] = b;
-            return b;
-        }
-    }
+	static int n,m; 
+	static int[] arr;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int t = Integer.parseInt(bf.readLine());
+		
+		for(int tc=1; tc<=t; tc++) {
+			st = new StringTokenizer(bf.readLine());
+			n = Integer.parseInt(st.nextToken()); // 정점의 수
+			m = Integer.parseInt(st.nextToken()); // 간선의 수
+			
+			make();
+			
+			for(int i=0; i<m; i++) {
+				st = new StringTokenizer(bf.readLine());
+				int from = Integer.parseInt(st.nextToken());
+				int to = Integer.parseInt(st.nextToken());
+				
+				union(from, to); // 합치기
+			}
+			
+			int ans = 0;
+			// 부모값이 자기 자신과 같으면 그룹
+			for(int i=1; i<arr.length; i++) {
+				if(arr[i] == i) {
+					ans++;
+				}
+			}
+			System.out.println("#"+tc+" "+ans);
+			
+		}
+	}
+	
+	// 모든 요소를 자기 자신으로 초기화
+	private static void make() {
+		arr = new int[n+1];
+		
+		for(int i=0; i<=n; i++) {
+			arr[i] = i;
+		}
+	}
+	
+	// a와 b를 합칠 수 있으면 true, 아니면 false
+	private static boolean union(int a, int b) {
+		int aRoot = find(a); //
+		int bRoot = find(b);
+		
+		if(aRoot == bRoot) return false;
+		
+		arr[bRoot] = aRoot;
+		return true;
+		
+	}
+	
+	// 최상위 부모노드 찾기
+	private static int find(int a) {
+		if(a == arr[a]) return a;
+		else {
+			// 최상위 노드를 배열의 값으로 설정
+			int b = find(arr[a]);
+			arr[a] = b;
+			return b;
+		}
+	}
 }
