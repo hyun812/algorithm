2ca3b036d979e2659e684bfcab7186431aa1a78c
SeungHyun <92029126+hyun812@users.noreply.github.com>
2023-10-11 17:30:48 +0900

    [Unrated] Title: [모의 SW 역량테스트] 미생물 격리, Time: 602 ms, Memory: 45,192 KB -BaekjoonHub

--- SWEA/Unrated/2382. ［모의 SW 역량테스트］ 미생물 격리/［모의 SW 역량테스트］ 미생물 격리.java
+++ SWEA/Unrated/2382. ［모의 SW 역량테스트］ 미생물 격리/［모의 SW 역량테스트］ 미생물 격리.java
@@ -2,27 +2,29 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-	static int n, m, k;
-	static ArrayList<mic> list;
-	static int ans;
-	static int[] dy = {0, -1, 1, 0, 0}; // 0  상 하 좌 우
-	static int[] dx = {0, 0, 0, -1, 1}; 
+	static int n,m,k;
+	static ArrayList<Bug> list;
+	static int[] dy = {0, -1, 1, 0, 0};
+	static int[] dx = {0, 0, 0, -1, 1};
 	
-	public static void main(String[] args) throws IOException {
+	static int ans;
+	
+	public static void main(String[] args) throws Exception {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		
-		int t = Integer.parseInt(bf.readLine().trim());
-		
-		for(int tc=1; tc<=t; tc++) {
+
+		// 테스트 케이스 개수
+		int t = Integer.parseInt(bf.readLine());
+
+		// 테스트 케이 개수 만큼 반복
+		for (int tc = 1; tc <= t; tc++) {
 			st = new StringTokenizer(bf.readLine());
+			n = Integer.parseInt(st.nextToken()); // 셀의 개수
+			m = Integer.parseInt(st.nextToken()); // 격리 시간
+			k = Integer.parseInt(st.nextToken()); // 미생물 군집의 개수
 			
-			n = Integer.parseInt(st.nextToken()); // 세로 n, 가로 n
-			m = Integer.parseInt(st.nextToken()); // 시간
-			k = Integer.parseInt(st.nextToken()); // k개의 미생물
-			
+			list = new ArrayList<>();
 			ans = 0;
-			list = new ArrayList<>(); // 군집들을 담을 list
 			
 			for(int i=0; i<k; i++) {
 				st = new StringTokenizer(bf.readLine());
@@ -31,63 +33,59 @@ public class Solution {
 				int cnt = Integer.parseInt(st.nextToken());
 				int dis = Integer.parseInt(st.nextToken());
 				
-				list.add(new mic(i, y, x, cnt, dis));
+				list.add(new Bug(i, y, x, cnt, dis));
 			}
-			move();
 			
-			System.out.println("#"+tc+" "+ans);
+			for(int i=0; i<m; i++) {
+				move();				
+				checkBug();
+			}
 			
+			for(int i=0; i<list.size(); i++) {
+				ans += list.get(i).cnt;
+			}
+			
+			System.out.println("#" + tc + " " + ans);
 		}
 	}
-	// List에 군집들 다 넣기
-	// 이동 다 시키기
-	// 이동 후 판단하기 > 
-	//	1. 약품이 칠해진 칸에 도착하면 방향 반대로 + 미생물 수 /2 해주기
-	// 	2. 군집이 합쳐졌을 때 미생물 수 합침 + 방향은 미생물이 많은 군집 따라감
-	//  List에 담은 군집들을 정렬시킴 (num, cnt)
 	
 	private static void move() {
-		for(int i=0; i<m; i++) { // 시간만큼 반복
-			
-			for(int j=0; j<list.size(); j++) {
-				mic m = list.get(j);
-				m.y = m.y+dy[m.dis];
-				m.x = m.x+dx[m.dis];
-			}
-			
-			check();
-		}
-		
 		for(int i=0; i<list.size(); i++) {
-			ans += list.get(i).cnt;
+			Bug bug = list.get(i);
+			
+			bug.y = bug.y + dy[bug.dis];
+			bug.x = bug.x + dx[bug.dis];
 		}
 	}
-
-	private static void check() {
-		// 정렬
-		for(mic m1 : list) {
-			if(divide(m1.y, m1.x)) {
-				m1.cnt = m1.cnt/2;
-				m1.dis = reverseDis(m1.dis);
+	
+	private static void checkBug() {
+		
+		// 군집을 돌면서 약품이 칠해진 공간에 진입 시 미생물 수를 나누기 2
+		// 군집이 겹쳤을 때 num을 같게 해주기
+		for(Bug a : list) {
+			if(!drugFillCheck(a.y, a.x)) {
+				a.cnt = a.cnt/2;
+				a.dis = reverseDis(a.dis);
 			}
 			
-			
-			for(mic m2 : list) {
-				if(m1 == m2) continue;
-				if(m1.y == m2.y && m1.x == m2.x) {
-					m2.num = m1.num;
+			for(Bug b : list) {
+				if(a == b) continue;  // 같은 군집이면 continue
+				if(a.y == b.y && a.x == b.x) {
+					b.num = a.num;
 				}
 			}
 		}
 		
+		// num 순으로 정렬
+		// num이 같으면 cnt순으로 정렬
 		Collections.sort(list);
 		
 		for(int i=0; i<list.size()-1; i++) {
-			mic m1 = list.get(i);
-			mic m2 = list.get(i+1);
+			Bug a = list.get(i);
+			Bug b = list.get(i+1);
 			
-			if(m1.num == m2.num) {
-				m1.cnt += m2.cnt;
+			if(a.num == b.num) {
+				a.cnt += b.cnt;
 				list.remove(i+1);
 				i--;
 			}
@@ -112,18 +110,17 @@ public class Solution {
 		}
 		return d;
 	}
-	
-	private static boolean divide(int y, int x) { // true면 약품이 칠해진 칸에 도착
+	private static boolean drugFillCheck(int y, int x) {
 		if(y>0 && y<n-1 && x>0 && x<n-1) {
-			return false;
+			return true;
 		}
-		return true;
+		return false;
 	}
 	
-	static class mic implements Comparable<mic>{
+	static class Bug implements Comparable<Bug> {
 		int num, y, x, cnt, dis;
 
-		public mic(int num, int y, int x, int cnt, int dis) {
+		public Bug(int num, int y, int x, int cnt, int dis) {
 			super();
 			this.num = num;
 			this.y = y;
@@ -133,16 +130,17 @@ public class Solution {
 		}
 
 		@Override
-		public int compareTo(mic o) { // num으로 정렬,  같으면 cnt로 정렬
-			if(this.num == o.num) {
-				return o.cnt-this.cnt;
-			}
-			return this.num-o.num;
+		public String toString() {
+			return "Bug [num=" + num + ", y=" + y + ", x=" + x + ", cnt=" + cnt + ", dis=" + dis + "]";
 		}
 
 		@Override
-		public String toString() {
-			return "mic [num=" + num + ", y=" + y + ", x=" + x + ", cnt=" + cnt + ", dis=" + dis + "]";
-		}
+		public int compareTo(Bug o) {
+			if(this.num != o.num) {
+				return this.num - o.num;
+			}else {
+				return o.cnt - this.cnt;
+			}
+		} 
 	}
 }
