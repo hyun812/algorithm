0f6ebe34c27b840c8e10c4ea833c90f3c5cf5ef9
SeungHyun <92029126+hyun812@users.noreply.github.com>
2023-10-11 16:10:25 +0900

    [Unrated] Title: [모의 SW 역량테스트] 무선 충전, Time: 141 ms, Memory: 21,472 KB -BaekjoonHub

--- SWEA/Unrated/5644. ［모의 SW 역량테스트］ 무선 충전/［모의 SW 역량테스트］ 무선 충전.java
+++ SWEA/Unrated/5644. ［모의 SW 역량테스트］ 무선 충전/［모의 SW 역량테스트］ 무선 충전.java
@@ -3,127 +3,139 @@ import java.util.*;
 
 public class Solution {
 	static int m, a;
-	static int[] aPos, bPos; // a와 b의 이동 방향 저장할 배열
+	static int[] user1, user2;
+	static ArrayList<BC> bcList;
+	static int y1, y2, x1, x2;
 	static int[] dy = {0, 0, 1, 0, -1}; // 상 우 하 좌
 	static int[] dx = {0, -1, 0, 1, 0}; // 상 우 하 좌
-	static ArrayList<apinfo> list; // 배터리 정보를 저장할 리스트
+
 	static int ans;
-	
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
+
+		// 테스트 케이스 개수
 		int t = Integer.parseInt(bf.readLine());
-		for(int tc=1; tc<=t; tc++) {
+
+		// 테스트 케이 개수 만큼 반복
+		for (int tc = 1; tc <= t; tc++) {
 			st = new StringTokenizer(bf.readLine());
-			m = Integer.parseInt(st.nextToken());
-			a = Integer.parseInt(st.nextToken());
-			
+			m = Integer.parseInt(st.nextToken()); // 이동 시간
+			a = Integer.parseInt(st.nextToken()); // bc의 개수
+
+			user1 = new int[m]; // A의 정보를 담을 배열
+			user2 = new int[m]; // B의 정보를 담을 배열
 			ans = 0;
-			aPos = new int[m];
-			bPos = new int[m];
-			list = new ArrayList<>();
+			bcList = new ArrayList<>();
+
 			st = new StringTokenizer(bf.readLine());
-			for(int i=0; i<m; i++) {
-				aPos[i] = Integer.parseInt(st.nextToken());
+			for (int i = 0; i < m; i++) {
+				user1[i] = Integer.parseInt(st.nextToken());
 			}
 			st = new StringTokenizer(bf.readLine());
-			for(int i=0; i<m; i++) {
-				bPos[i] = Integer.parseInt(st.nextToken());
+			for (int i = 0; i < m; i++) {
+				user2[i] = Integer.parseInt(st.nextToken());
 			}
-			
-			for(int i=0; i<a; i++) {
+
+			for (int i = 0; i < a; i++) {
 				st = new StringTokenizer(bf.readLine());
 				int y = Integer.parseInt(st.nextToken());
 				int x = Integer.parseInt(st.nextToken());
 				int cover = Integer.parseInt(st.nextToken());
-				int perfor = Integer.parseInt(st.nextToken());
-				
-				list.add(new apinfo(y, x, cover, perfor));
+				int power = Integer.parseInt(st.nextToken());
+
+				bcList.add(new BC(y, x, cover, power));
 			}
-			
 			move();
 			
-			System.out.println("#"+tc+" "+ans);
+			System.out.println("#" + tc + " " + ans);
 		}
 	}
-	// a와 b 둘다 한칸씩 옮긴 뒤에 계산하기
-	// 같은 충전 범위에 들어온다면 체크해야하는데
-	// 둘다 충전범위 하나에만 들어와 있다면 균등하게 배분
-	// 2개이상의 충전범위에 들어와 있다면 계산해서 배분
-	private static void move() { // 이동할 함수
-		 
-		int[] axy = new int[] {1, 1}; // a와 b의 포지션
-		int[] bxy = new int[] {10, 10};
+
+	// 이동하기
+	private static void move() {
+		y1 = x1 = 1;
+		y2 = x2 = 10;
 		
-		check(axy, bxy); // 초기상태에서 확인
+		getBattery();
 		
-		for(int i=0; i<m; i++) { // m만큼 반복
-			
-			// 좌표 이동
-			axy[0] = axy[0]+dy[aPos[i]];
-			axy[1] = axy[1]+dx[aPos[i]];
-			
-			bxy[0] = bxy[0]+dy[bPos[i]];
-			bxy[1] = bxy[1]+dx[bPos[i]];
-			
-			check(axy, bxy);
+		for (int i = 0; i < m; i++) {
+
+			// 사용자 A 이동시키기
+			y1 += dy[user1[i]];
+			x1 += dx[user1[i]];
+
+			// 사용자 B 이동시키기
+			y2 += dy[user2[i]];
+			x2 += dx[user2[i]];
+
+			getBattery();
+
 		}
 	}
-	
-	private static void check(int[] axy, int[] bxy) { // 현재 좌표 기반 최적 배터리 선택하는 함수
-		ArrayList<Integer> am = new ArrayList<>();
-		ArrayList<Integer> bm = new ArrayList<>();
-		for(int i=0; i<a; i++) { // 배터리 수만큼 반복
-			apinfo ai = list.get(i);
-			// 범위 확인해서 리스트에 추가
-			if(Math.abs(ai.y - axy[0])+Math.abs(ai.x - axy[1]) <= ai.cover) {
-				am.add(i);
-			}
+
+	// 배터리 확인 후 충전한 양 계산하기
+	private static void getBattery() {
+		ArrayList<Integer> cv1 = new ArrayList<>();
+		ArrayList<Integer> cv2 = new ArrayList<>();
+		
+		for(int i=0; i<a; i++) { // bc의 개수만큼 반복
+			BC bc = bcList.get(i);
 			
-			if(Math.abs(ai.y - bxy[0])+Math.abs(ai.x - bxy[1]) <= ai.cover) {
-				bm.add(i);
+			// 사용자 A가 해당 bc의 범위 안에 있을 때 list에 추가
+			if(getDistance(y1, x1, bc.y, bc.x) <= bc.cover) {
+				cv1.add(i);
+			}
+			// 사용자 B가 해당 bc의 범위 안에 있을 때 list에 추가
+			if(getDistance(y2, x2, bc.y, bc.x) <= bc.cover) {
+				cv2.add(i);
 			}
 		}
-		int max =0;
-		if(am.size() > 0 && bm.size() > 0) { // 둘다 1개 이상 접속 가능하면
-			for(Integer a : am) {
-				for(Integer b : bm) {
+		
+		int max = 0;
+		
+		// 둘다 1보다 크면
+		if(cv1.size() >= 1 && cv2.size() >= 1) {
+			for(Integer i : cv1) {
+				for(Integer j : cv2) {
 					int temp = 0;
-					if(a==b) { // 같은 배터리이면 
-						temp += list.get(a).perfor;
-					}else { // 다른 배터리이면
-						temp += list.get(a).perfor;
-						temp += list.get(b).perfor;
+					if(i==j) { // 같은 범위안에 있으면
+						temp += bcList.get(i).power;
+					}else {
+						temp += bcList.get(i).power;
+						temp += bcList.get(j).power;
 					}
 					max = Math.max(max, temp);
 				}
 			}
-		}else if(am.size() > 0) { // 접속가능한 배터리가 a만 있을 때
-			for(Integer a : am) {
-				max = Math.max(max, list.get(a).perfor);	
+		}else if(cv1.size() >= 1) {
+			for(Integer i : cv1) {
+				max = Math.max(max, bcList.get(i).power);
 			}
-		}else if(bm.size() > 0) { // 접속가능한 배터리가 b만 있을 때
-			for(Integer b : bm) {
-				max = Math.max(max, list.get(b).perfor);	
+		}else if(cv2.size() >= 1) {
+			for(Integer i : cv2) {
+				max = Math.max(max, bcList.get(i).power);
 			}
 		}
+		
 		ans += max;
 	}
-	
-	
-	static class apinfo {
-		int y, x, cover, perfor;
 
-		public apinfo(int y, int x, int cover, int perfor) {
+	// 거리 계산 함수
+	private static int getDistance(int y, int x, int bcy, int bcx) {
+		return Math.abs(bcy - y) + Math.abs(bcx - x);
+	}
+
+	static class BC {
+		int y, x, cover, power;
+
+		public BC(int y, int x, int cover, int power) {
 			super();
 			this.y = y;
 			this.x = x;
 			this.cover = cover;
-			this.perfor = perfor;
-		}
-		@Override
-		public String toString() {
-			return "apinfo [y=" + y + ", x=" + x + ", cover=" + cover + ", perfor=" + perfor + "]";
+			this.power = power;
 		}
 	}
 }
