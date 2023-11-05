3eef6c73d16e0587cad27ffa30b59cbcfa5f766e
SeungHyun <92029126+hyun812@users.noreply.github.com>
2023-11-05 17:33:16 +0900

    [Unrated] Title: [SW Test 샘플문제] 프로세서 연결하기, Time: 132 ms, Memory: 21,832 KB -BaekjoonHub

--- SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
+++ SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
@@ -2,134 +2,120 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-	static int n;
-	static int[][] arr;
-	static ArrayList<int[]> list;
-	static int ans;
-	static int maxCount;
-	public static void main(String[] args) throws Exception {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		
-		int t=  Integer.parseInt(bf.readLine());
-		
-		for(int tc=1; tc<=t; tc++) {
-			n = Integer.parseInt(bf.readLine());
-			
-			arr = new int[n][n];
-			list = new ArrayList<>();
-			for(int i=0; i<n; i++) {
-				st = new StringTokenizer(bf.readLine());
-				for(int j=0; j<n; j++) {
-					arr[i][j] = Integer.parseInt(st.nextToken());
-					if(arr[i][j] == 1) {
-						if(i==0 || j==0 || i==n-1 || j==n-1) continue;
-						list.add(new int[] {i, j});
-					}
-				}
-			}
-			
-			maxCount = Integer.MIN_VALUE;
-			ans = Integer.MAX_VALUE;
-			
-			powerset(0, 0);
-			
-			sb.append("#").append(tc).append(" ").append(ans).append("\n");
-		}
-		System.out.println(sb.toString());
-	}
-	
-	private static void powerset(int cnt, int count) {
-		if(count + (list.size()-cnt) < maxCount) return;
-		
-		
-		if(cnt == list.size()) {
-			if(maxCount < count) {
-				maxCount = count;
-				
-				int temp = 0;
-				for(int i=0; i<n; i++) {
-					for(int j=0; j<n; j++) {
-						if(arr[i][j] != 2) continue;
-						temp++;
-					}
-				}
-				ans = temp;
-			}else if(maxCount == count) {
-				int temp = 0;
-				for(int i=0; i<n; i++) {
-					for(int j=0; j<n; j++) {
-						if(arr[i][j] != 2) continue;
-						temp++;
-					}
-				}
-				ans = Math.min(ans, temp);
-			}
-			
-			return;
-		}
-		
-		// 4방향 중 어디로 연결할 지
-		for(int j=0; j<4; j++) {
-			// 해당방향으로 연결할 수 있는지 확인
-			if(!isConnect(cnt, j)) continue;
-				
-			// 연결하기
-			connect(cnt, j, 2);
-			powerset(cnt+1, count+1);
-			connect(cnt, j, 0);
-		}
-		powerset(cnt+1, count);		
-	}
-	
-	static int[] dy = {-1, 1, 0, 0};
-	static int[] dx = {0, 0, -1, 1};
-	// 연결 할 수 있는지 판단
-	private static boolean isConnect(int idx, int dis) {
-		int[] target = list.get(idx);
-		int y = target[0];
-		int x = target[1];
-		
-		
-		int ny = y;
-		int nx = x;
-		while(true) {
-			ny += dy[dis];
-			nx += dx[dis];
-			
-			if(!outOfIdx(ny, nx)) break;
-			if(arr[ny][nx] == 1 || arr[ny][nx] == 2) return false;
-		}
-		
-		return true;
-	}
-	
-	private static void connect(int idx, int dis, int value) {
-		int[] target = list.get(idx);
-		int y = target[0];
-		int x = target[1];
-		
-		
-		int ny = y;
-		int nx = x;
-		while(true) {
-			ny += dy[dis];
-			nx += dx[dis];
-			
-			if(!outOfIdx(ny, nx)) break;
-			arr[ny][nx] = value;
-		}
-		return;
-	}
-	
-	private static boolean outOfIdx(int ny, int nx) {
-		if(ny>=0 && ny<n && nx>=0 && nx<n) {
-			return true;
-		}
-		return false;
-	}
-	
-	
-	
+    static int n, max, totalCnt, min;
+    static int[][] arr;
+    static ArrayList<int[]> list;
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int t = Integer.parseInt(bf.readLine());
+
+        for(int tc=1; tc<=t; tc++){
+            n = Integer.parseInt(bf.readLine());
+            arr = new int[n][n];
+            list = new ArrayList<>();
+            max = 0;
+            min = Integer.MAX_VALUE;
+            totalCnt = 0; // 연결해야하는 코어 개수
+
+
+            for(int i=0; i<n; i++){
+                st = new StringTokenizer(bf.readLine());
+                for(int j=0; j<n; j++){
+                    arr[i][j] = Integer.parseInt(st.nextToken());
+                    if(arr[i][j] == 1){
+                        if(i == 0 || i == n-1 || j == 0 || j == n-1) continue;
+                        list.add(new int[] {i, j});
+                        totalCnt++;
+                    }
+                }
+            }
+
+            powerSet(0, 0);
+            sb.append("#").append(tc).append(" ").append(min).append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+    // 부분집합 코어를 선택(4방향 시도)/비선택
+    private static void powerSet(int start, int corent){ // start : 코어의 인덱스 , coreCnt : 연결된 코어 갯수
+        // 가지치기 : 현재까지 연결된 코어수 + 남은 코어수 < 임시 최대 코어 연결수
+        if(corent + (totalCnt-start) < max) return;
+        
+        // 기저조건 처리
+        if(start == totalCnt){
+            int res=  getLength(); // 놓아진 전선 길이의 합
+            if(max < corent){
+                max = corent;
+                min = res;
+            }else if (max == corent){
+                if(min > res){
+                    min = res;
+                }
+            }
+            return;
+        }
+
+        // 현재 코어 선택 ( 4방향 시도)
+        for(int i=0; i<4; i++){
+            // 현재 코어의 위치에서 해당 방향으로 전선 놓기가 가능한지 체크
+            if(!check(list.get(start), i)) continue;
+
+            fill(list.get(start), i, 2); // 가능하다면 전선 놓기
+            powerSet(start+1, corent+1); // 다음 코어로 가기
+            fill(list.get(start), i, 0); // 새로운 방향을 시도하기 위해 놓았던 전선 지우기
+        }
+
+        powerSet(start+1, corent);
+    }
+
+    private static boolean check(int[] target, int dir){ // 4방향 중 갈 수 있는 방향인지 boolean
+        int ny = target[0] + dy[dir];
+        int nx = target[1] + dx[dir];
+
+        while (outOfIdx(ny, nx)){
+            if(arr[ny][nx] != 0) return false;
+
+            ny += dy[dir];
+            nx += dx[dir];
+        }
+
+        return true;
+    }
+
+    static int[] dy = {-1, 1, 0, 0};
+    static int[] dx = {0, 0, -1, 1};
+
+
+    private static void fill(int[] target, int dir, int value){
+        int ny = target[0] + dy[dir];
+        int nx = target[1] + dx[dir];
+
+        while (outOfIdx(ny, nx)){
+            arr[ny][nx] = value;
+            ny += dy[dir];
+            nx += dx[dir];
+        }
+    }
+
+    private static int getLength(){
+        int lCnt = 0;
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(arr[i][j] == 2){
+                    lCnt++;
+                }
+            }
+        }
+        return lCnt;
+    }
+
+    private static boolean outOfIdx(int y, int x){
+        if(y>=0 && y<n && x>=0 && x<n){
+            return true;
+        }
+        return false;
+    }
 }
