02c593417ab1d6d73fcc85720cf19939738847a5
SeungHyun <92029126+hyun812@users.noreply.github.com>
2023-11-05 18:32:55 +0900

    [Unrated] Title: [모의 SW 역량테스트] 탈주범 검거, Time: 133 ms, Memory: 23,308 KB -BaekjoonHub

--- SWEA/Unrated/1953. ［모의 SW 역량테스트］ 탈주범 검거/［모의 SW 역량테스트］ 탈주범 검거.java
+++ SWEA/Unrated/1953. ［모의 SW 역량테스트］ 탈주범 검거/［모의 SW 역량테스트］ 탈주범 검거.java
@@ -2,156 +2,101 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-    static int[][] arr;
-    static int[][] visited;
-    static int n,m,r,c,l;
-    static int ans;
+	static int n, m, r, c, l;
+	static String[][] arr;
+	static int ans;
+	public static void main(String[] args) throws Exception {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		int t=  Integer.parseInt(bf.readLine());
+		
+		for(int tc=1; tc<=t; tc++) {
+			st = new StringTokenizer(bf.readLine());
+			n = Integer.parseInt(st.nextToken());
+			m = Integer.parseInt(st.nextToken());
+			r = Integer.parseInt(st.nextToken());
+			c = Integer.parseInt(st.nextToken());
+			l = Integer.parseInt(st.nextToken());
+			
+			ans = 1;
+			arr = new String[n][m];
+			for(int i=0; i<n; i++) {
+				st = new StringTokenizer(bf.readLine());
+				for(int j=0; j<m; j++) {
+					arr[i][j] = st.nextToken();
+				}
+			}
+			
+			bfs();
+			
+			sb.append("#").append(tc).append(" ").append(ans).append("\n");
+		}
+		System.out.println(sb.toString());
+	}
+	
+	static int[] dy = {-1, 1, 0, 0};
+	static int[] dx = {0, 0, -1, 1};
+	private static void bfs() {
+		Queue<person> q = new ArrayDeque<>();
+		boolean[][] visited = new boolean[n][m];
+		
+		q.add(new person(r,c, 1));
+		visited[r][c] = true;
+		
+		while(!q.isEmpty()) {
+			person poll = q.poll();
+			int y = poll.y;
+			int x = poll.x;
+			int time = poll.time;
+			String target = arr[y][x];
+			
+			if(time == l) break;
+			
+			for(int i=0; i<4; i++) {
+				// 갈 수 있는 방향인지 확인
+				if(i==0 && !("1247".contains(target))) continue;
+				if(i==1 && !("1256".contains(target))) continue;
+				if(i==2 && !("1367".contains(target))) continue;
+				if(i==3 && !("1345".contains(target))) continue;
+				
+				int ny = y + dy[i];
+				int nx = x + dx[i];
+				
+				if(!outOfIdx(ny, nx)) continue;
+				if(visited[ny][nx]) continue;
+				String nt = arr[ny][nx];
+				
+				if(nt.equals("0")) continue;
+				
+				if(i==0 && "347".contains(nt)) continue;
+				if(i==1 && "356".contains(nt)) continue;
+				if(i==2 && "267".contains(nt)) continue;
+				if(i==3 && "245".contains(nt)) continue;
+				
+				q.add(new person (ny, nx, time+1));
+				visited[ny][nx] = true;
+				ans++;
+			}
+		}
+	}
+	
+	private static boolean outOfIdx(int ny, int nx) {
+		if(ny>=0 && ny<n && nx>=0 && nx<m) {
+			return true;
+		}
+		return false;
+	}
+	
+	static class person{
+		int y, x, time;
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
-        StringTokenizer st;
-        int t = Integer.parseInt(bf.readLine());
-
-        for(int tc=1; tc<=t; tc++){
-            st = new StringTokenizer(bf.readLine());
-            n = Integer.parseInt(st.nextToken());
-            m = Integer.parseInt(st.nextToken());
-            r = Integer.parseInt(st.nextToken());
-            c = Integer.parseInt(st.nextToken());
-            l = Integer.parseInt(st.nextToken());
-
-            ans = 1;
-            arr = new int[n][m];
-            visited = new int[n][m];
-            for(int i=0; i<n; i++){
-                st = new StringTokenizer(bf.readLine());
-                for(int j=0; j<m; j++){
-                    arr[i][j] = Integer.parseInt(st.nextToken());
-                }
-            }
-            if(l == 1){
-                System.out.println("#"+tc+" "+ans);
-            }else{
-                dfs();
-                System.out.println("#"+tc+" "+ans);
-            }
-
-        }
-    }
-
-    private static void dfs(){
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[] {r, c});
-        visited[r][c] = 1;
-
-        while (!q.isEmpty()){
-            int[] poll = q.poll();
-            int y = poll[0];
-            int x = poll[1];
-            int target = arr[y][x];
-
-            dirRotate(target);
-            for(int i=0; i<dx.length; i++){
-                int ny = y + dy[i];
-                int nx = x + dx[i];
-
-                if(!outofidx(ny, nx)) continue;
-                if(visited[ny][nx] != 0) continue;
-
-                int ntar = arr[ny][nx];
-
-                if(ntar != 0 && check(dy[i], dx[i], ntar)){
-                    visited[ny][nx] = visited[y][x] +1;
-                    ans++;
-                    if(visited[ny][nx] == l) continue;
-
-                    q.add(new int[] {ny, nx});
-                }
-            }
-        }
-    }
-
-    private static boolean check(int dy1, int dx1, int ntar){
-        if(ntar == 1) return true;
-        if(dy1 == -1 && dx1 == 0){ // 위
-            if(ntar == 2 || ntar == 5 || ntar == 6){
-                return true;
-            }
-        }else if(dy1 == 1 && dx1 == 0){ // 아래
-            if(ntar == 2 || ntar == 4 || ntar == 7){
-                return true;
-            }
-        }else if(dy1 == 0 && dx1 == -1){ // 왼쪽
-            if(ntar == 3 || ntar == 4 || ntar == 5){
-                return true;
-            }
-        }else if(dy1 == 0 && dx1 == 1){ // 오른쪽
-            if(ntar == 3 || ntar == 6 || ntar == 7){
-                return true;
-            }
-        }
-        return false;
-    }
-    // 0이아니고 범위 벗어나지않고
-    // 갈수 있는 길이면
-    // 1이면 위에는 2,5,6 올 수 있음
-    //      아래에는 2,4,7 올 수 있음
-    //      왼쪽에는 3,4,5 올 수 있음
-    //      오른쪽에는 3,6,7 올 수 있음
-
-    // 2이면 위에는 2,5,6
-    //      아래에는 2,4,7
-
-    // 3이면 왼쪽에는 3,4,5
-//          오른쪽에는 3,6,7
-
-    static int[] dy;
-    static int[] dx;
-    private static void dirRotate(int target) {
-        switch (target){
-            case 1 :
-                dy = new int[] {-1, 1, 0, 0};
-                dx = new int[] {0, 0, -1, 1};
-                break;
-            case 2 :
-                dy = new int[] {-1, 1};
-                dx = new int[] {0, 0};
-                break;
-            case 3 :
-                dy = new int[] {0, 0};
-                dx = new int[] {-1, 1};
-                break;
-            case 4 :
-                dy = new int[] {-1, 0};
-                dx = new int[] {0, 1};
-                break;
-            case 5 :
-                dy = new int[] {1, 0};
-                dx = new int[] {0, 1};
-                break;
-            case 6 :
-                dy = new int[] {1, 0};
-                dx = new int[] {0, -1};
-                break;
-            case 7 :
-                dy = new int[] {-1, 0};
-                dx = new int[] {0, -1};
-                break;
-        }
-    }
-
-    private static boolean outofidx(int y, int x){
-        if(y >=0 && y<n && x>=0 && x<m){
-            return true;
-        }
-        return false;
-    }
+		public person(int y, int x, int time) {
+			super();
+			this.y = y;
+			this.x = x;
+			this.time = time;
+		}
+	}
 }
-
-//5 6 2 1 3
-//0 0 5 3 6 0
-//0 0 2 0 2 0
-//3 3 1 3 7 0
-//0 0 0 0 0 0
-//0 0 0 0 0 0
