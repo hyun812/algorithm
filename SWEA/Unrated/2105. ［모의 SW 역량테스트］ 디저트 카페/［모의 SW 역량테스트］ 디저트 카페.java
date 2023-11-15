e615b18dccc38d796a8b6dcfabd360ae1e686246
SeungHyun <92029126+hyun812@users.noreply.github.com>
2023-11-16 00:02:11 +0900

    [Unrated] Title: [모의 SW 역량테스트] 디저트 카페, Time: 164 ms, Memory: 23,700 KB -BaekjoonHub

--- SWEA/Unrated/2105. ［모의 SW 역량테스트］ 디저트 카페/［모의 SW 역량테스트］ 디저트 카페.java
+++ SWEA/Unrated/2105. ［모의 SW 역량테스트］ 디저트 카페/［모의 SW 역량테스트］ 디저트 카페.java
@@ -4,78 +4,79 @@ import java.util.*;
 public class Solution {
 	static int n;
 	static int[][] arr;
-	static boolean[] desert;
-	static boolean[][] visited;
-	static int ans;
 	static int startY, startX;
-	
+	static int ans;
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
-		
-		int t=  Integer.parseInt(bf.readLine());
-		
-		for(int tc=1; tc<=t; tc++) {
-			n = Integer.parseInt(bf.readLine());
+		StringTokenizer st;
+
+		int t = Integer.parseInt(bf.readLine());
+
+		for (int tc = 1; tc <= t; tc++) {
+			st = new StringTokenizer(bf.readLine());
+
+			n = Integer.parseInt(st.nextToken());
+
 			arr = new int[n][n];
-			ans = -1;
-			for(int i=0; i<n; i++) {
+			ans = Integer.MIN_VALUE;
+			for (int i = 0; i < n; i++) {
 				st = new StringTokenizer(bf.readLine());
-				for(int j=0; j<n; j++) {
+				for (int j = 0; j < n; j++) {
 					arr[i][j] = Integer.parseInt(st.nextToken());
 				}
 			}
-			
-			desert = new boolean[101];
-			visited = new boolean[n][n];
-			for(int i=0; i<n; i++) {
-				for(int j=0; j<n; j++) {
+
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					boolean[] visited = new boolean[101]; // 같은 디저트를 먹었는지 확인하기 위한 배열
 					startY = i;
 					startX = j;
-					
-					desert[arr[i][j]]= true;
-					visited[i][j] = true;
-					dfs(i, j, 1, 0);
-					desert[arr[i][j]]= false;
-					visited[i][j] = false;
+					int target = arr[startY][startX];
+					visited[target] = true;
+					dfs(i, j, 0, 1, visited);
 				}
+			}
+			if (ans == Integer.MIN_VALUE) {
+				ans = -1;
 			}
 			sb.append("#").append(tc).append(" ").append(ans).append("\n");
 		}
-		
 		System.out.println(sb.toString());
 	}
-	
-	static int[] dy = {-1, 1, 1, -1};
-	static int[] dx = {1, 1, -1, -1};
-	
-	private static void dfs(int y, int x, int count, int preD) {
-		
-		for(int i=preD; i<4; i++) {
+
+	static int[] dy = { -1, -1, 1, 1 };
+	static int[] dx = { -1, 1, 1, -1 };
+
+	private static void dfs(int y, int x, int dis, int cnt, boolean[] visited) {
+
+		// 사각형 모양을 만들기 위해
+		for (int i = dis; i < 4; i++) {
 			int ny = y + dy[i];
 			int nx = x + dx[i];
-			
-			if(!outOfIdx(ny, nx)) continue;
-			
-			if(ny == startY && nx == startX && count > 2) {
-				ans = Math.max(ans, count);
+
+			// 다시 원점으로 돌아와야 함
+			if (ny == startY && nx == startX && cnt > 2) {
+				ans = Math.max(ans, cnt);
 				return;
 			}
-			
-			if(visited[ny][nx]) continue; // 방문한 적이 있다면
-			if(desert[arr[ny][nx]]) continue; // 이미 같은 종류를 먹었으면
-			
-			desert[arr[ny][nx]] = true;
-			visited[ny][nx] = true;
-			dfs(ny, nx, count+1, i);
-			desert[arr[ny][nx]] = false;
-			visited[ny][nx] = false;
+
+			if (!outOfIdx(ny, nx))
+				continue; // 범위 벗어나면
+
+			int target = arr[ny][nx];
+			if (visited[target])
+				continue; // 이미 먹은 디저트라면
+
+			visited[target] = true;
+			dfs(ny, nx, i, cnt + 1, visited);
+			visited[target] = false;
 		}
 	}
-	
+
 	private static boolean outOfIdx(int ny, int nx) {
-		if(ny>=0 && ny<n && nx>=0 && nx<n) {
+		if (ny >= 0 && ny < n && nx >= 0 && nx < n) {
 			return true;
 		}
 		return false;
