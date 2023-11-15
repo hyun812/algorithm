f32e1b441f877e576813e2bfbf311d8e7a6da1e9
SeungHyun <92029126+hyun812@users.noreply.github.com>
2023-11-15 16:04:08 +0900

    [Gold IV] Title: 게리맨더링, Time: 120 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Gold/17471. 게리맨더링/게리맨더링.java
+++ 백준/Gold/17471. 게리맨더링/게리맨더링.java
@@ -1,123 +1,124 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-
-    static int[] pnums;
-    static boolean[] visited;
-    static ArrayList<Integer>[] list;
-    static int n;
-    static int ans;
-    public static void main(String[] args) throws Exception {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
-        n = Integer.parseInt(bf.readLine());
-
-        list = new ArrayList[n];
-        for(int i=0; i<n; i++){
-            list[i] = new ArrayList<>();
-        }
-
-        pnums = new int[n];
-        visited = new boolean[n];
-        ans = Integer.MAX_VALUE;
-
-        st = new StringTokenizer(bf.readLine());
-        for(int i=0; i<n; i++){
-            pnums[i] = Integer.parseInt(st.nextToken());
-        }
-
-        for(int i=0; i<n; i++){
-            st = new StringTokenizer(bf.readLine());
-            int end = Integer.parseInt(st.nextToken());
-            for(int j=0; j<end; j++){
-                int to = Integer.parseInt(st.nextToken());
-
-                list[i].add(to-1);
-            }
-        }
-
-        powerSet(0);
-
-        if(ans == Integer.MAX_VALUE){
-            System.out.println("-1");
-        }else{
-            System.out.println(ans);
-        }
-    }
-
-    private static void powerSet(int cnt){
-        if(cnt == n){
-            ArrayList<Integer> al1 = new ArrayList<>();
-            ArrayList<Integer> al2 = new ArrayList<>();
-
-            for(int i=0; i< n; i++){
-                if(visited[i]){
-                    al1.add(i);
-                }
-            }
-
-            for(int i=0; i< n; i++){
-                if(!visited[i]){
-                    al2.add(i);
-                }
-            }
-
-            if(al1.isEmpty() || al2.isEmpty()){
-                return;
-            }
-
-            if(check(al1) && check(al2)){
-                diff(al1, al2);
-            }
-            return;
-        }
-
-        visited[cnt] = true;
-        powerSet(cnt+1);
-        visited[cnt] = false;
-        powerSet(cnt+1);
-    }
-
-    private static boolean check(ArrayList<Integer> li){
-
-        Queue<Integer> q = new LinkedList<>();
-        boolean[] ckarr = new boolean[n];
-        ckarr[li.get(0)] = true;
-        q.add(li.get(0));
-
-        int count = 1;
-
-        while (!q.isEmpty()){
-            int poll = q.poll();
-            for(int i=0; i<list[poll].size(); i++){
-                int ck = list[poll].get(i);
-                if(li.contains(ck) && !ckarr[ck]){
-                    ckarr[ck] = true;
-                    q.add(ck);
-                    count++;
-                }
-            }
-        }
-
-        if(li.size() == count){
-            return true;
-        }
-        return false;
-    }
-
-    private static void diff(ArrayList<Integer> al1, ArrayList<Integer> al2){
-        int cnt1 = 0;
-        int cnt2 = 0;
-        for(Integer i : al1){
-            cnt1 += pnums[i];
-        }
-        for(Integer i : al2){
-            cnt2 += pnums[i];
-        }
-
-        ans = Math.min(ans, Math.abs(cnt1-cnt2));
-    }
+	static int n;
+	static int[] person;
+	static ArrayList<Integer>[] list;
+	static boolean[] visited;
+	static int ans;
+	public static void main(String[] args) throws Exception {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		n = Integer.parseInt(bf.readLine());
+		visited = new boolean[n+1];
+		person = new int[n+1]; // 각 구역의 인구수 저장
+		st = new StringTokenizer(bf.readLine());
+		for(int i=1; i<=n; i++) {
+			person[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		list = new ArrayList[n+1];
+		for(int i=0; i<=n; i++) {
+			list[i] = new ArrayList<>();
+		}
+		
+		for(int i=1; i<=n; i++) {
+			st = new StringTokenizer(bf.readLine());
+			int cnt = Integer.parseInt(st.nextToken());
+			for(int j=0; j<cnt; j++) {
+				int to = Integer.parseInt(st.nextToken());
+				// 양방향이므로
+				list[i].add(to);
+				list[to].add(i);
+			}
+		}
+		ans = Integer.MAX_VALUE;
+		powerset(1);
+		
+		if(ans == Integer.MAX_VALUE) {
+			System.out.println(-1);
+		}else {
+			System.out.println(ans);	
+		}
+	}
+	
+	// 부분집합 ( 공징합이 아닌 )
+	private static void powerset(int cnt) {
+		if(cnt == n+1) {
+			// 두 선거구로 나눔
+			ArrayList<Integer> l1 = new ArrayList<>();
+			ArrayList<Integer> l2 = new ArrayList<>();
+			int size1 = 0;
+			int size2 = 0;
+			for(int i=1; i<=n; i++) {
+				if(visited[i]) {
+					l1.add(i);
+					size1 += person[i];
+				}else {
+					l2.add(i);
+					size2 += person[i];
+				}
+			}
+			// 공집합이면 return
+			if(l1.size() == 0 || l2.size() == 0) return;
+			
+			// 각각 선거구가 연결되어있는지 확인
+			if(bfs(l1) && bfs(l2)) {
+				ans = Math.min(ans, Math.abs(size2-size1));
+			}
+			return;
+		}
+		
+		visited[cnt] = true;
+		powerset(cnt+1);
+		visited[cnt] = false;
+		powerset(cnt+1);
+	}
+	
+	private static boolean bfs(ArrayList<Integer> l) {
+		// 전달받은 리스트의 원소값을 큐에 넣고
+		// bfs 돌림
+		// 갈 수 있는 곳 안에 포함되지 않는다면 못가는애임
+		
+		Queue<Integer> q = new ArrayDeque<>();
+		boolean[] visit = new boolean[n+1];
+		
+		int start = l.get(0); 
+		q.add(start);
+		visit[start] = true;
+		
+		while(!q.isEmpty()) {
+			int poll = q.poll();
+			
+			for(int i=0; i<list[poll].size(); i++) {
+				int go = list[poll].get(i);
+				
+				if(visit[go]) continue;
+				if(!l.contains(go)) continue;
+				q.add(go);
+				visit[go] = true;
+			}
+		}
+		
+		for(int i : l) {
+			if(!visit[i]) {
+				return false;
+			}
+		}
+		
+		return true;
+		
+	}
+	
+	// 인구의 차이를 최소로 하는 값
+	
+	
+	
+	
+	
+	
+	
+	
 }
-
