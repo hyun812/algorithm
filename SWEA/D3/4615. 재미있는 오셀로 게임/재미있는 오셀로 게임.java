6f9f564a1d6a1d9882aeaf67e4f1c848507245d8
SeungHyun <92029126+hyun812@users.noreply.github.com>
2023-11-16 14:43:39 +0900

    [D3] Title: 재미있는 오셀로 게임, Time: 118 ms, Memory: 21,392 KB -BaekjoonHub

--- SWEA/D3/4615. 재미있는 오셀로 게임/재미있는 오셀로 게임.java
+++ SWEA/D3/4615. 재미있는 오셀로 게임/재미있는 오셀로 게임.java
@@ -2,114 +2,123 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-    static int n, m;
-    static int[][] arr;
-    static int ans;
+	static int n, m;
+	static int[][] arr;
+	static int ansB, ansW;
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
+	public static void main(String[] args) throws Exception {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
 
-        int t = Integer.parseInt(bf.readLine());
+		int t = Integer.parseInt(bf.readLine());
 
-        for (int tc = 1; tc <= t; tc++) {
-            st = new StringTokenizer(bf.readLine());
-            n = Integer.parseInt(st.nextToken()); // 보드 한변의 길이
-            m = Integer.parseInt(st.nextToken()); // 돌을 놓는 횟수
+		for (int tc = 1; tc <= t; tc++) {
+			st = new StringTokenizer(bf.readLine());
 
-            arr = new int[n][n];
+			n = Integer.parseInt(st.nextToken()); // 한변의 길이
+			m = Integer.parseInt(st.nextToken()); // 돌을 놓는 횟수
 
-            int center = n / 2; // 2 3 4
+			arr = new int[n][n];
+			ansB = ansW = 0;
 
-            arr[center - 1][center - 1] = 2;
-            arr[center - 1][center] = 1;
-            arr[center][center - 1] = 1;
-            arr[center][center] = 2;
+			// 초기 세팅
+			int temp = (n / 2) - 1;
+			arr[temp][temp] = 2;
+			arr[temp][temp + 1] = 1;
+			arr[temp + 1][temp] = 1;
+			arr[temp + 1][temp + 1] = 2;
 
-            for (int i = 0; i < m; i++) {
-                st = new StringTokenizer(bf.readLine());
-                int y = Integer.parseInt(st.nextToken()) - 1;
-                int x = Integer.parseInt(st.nextToken()) - 1;
-                int type = Integer.parseInt(st.nextToken()); // 1이면 흑 2면 백
+			for (int j = 0; j < m; j++) {
+				st = new StringTokenizer(bf.readLine());
+				int y = Integer.parseInt(st.nextToken()) - 1;
+				int x = Integer.parseInt(st.nextToken()) - 1;
+				int type = Integer.parseInt(st.nextToken()); // 1이면 흑, 2 이면 백
 
-                oneStep(y, x, type);
-            }
+				doit(y, x, type);
 
-            int bans = 0;
-            int wans = 0;
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    if (arr[i][j] == 1) {
-                        bans++;
-                    } else if (arr[i][j] == 2) {
-                        wans++;
-                    }
-                }
-            }
+			}
 
-            sb.append("#").append(tc).append(" ").append(bans).append(" ").append(wans).append("\n");
+			checkBW();
+			sb.append("#").append(tc).append(" ").append(ansB).append(" ").append(ansW).append("\n");
+		}
+		System.out.println(sb.toString());
+	}
 
-        }
-        System.out.println(sb.toString());
-    }
+	static int[] dy = { -1, 1, 0, 0, -1, -1, 1, 1 };
+	static int[] dx = { 0, 0, -1, 1, -1, 1, -1, 1 };
 
-    // 자리에 놓고 8방 탐색 후에 다른색돌이있으면 그끝에 내돌이있는지 확인하기
+	private static void doit(int y, int x, int type) {
+		// 입력 받은 자리에 돌 놓기
+		arr[y][x] = type;
 
-    static int[] dy = { -1, 1, 0, 0, -1, 1, 1, -1 };
-    static int[] dx = { 0, 0, -1, 1, 1, 1, -1, -1 };
+		// 8방향 탐색
+		for (int i = 0; i < 8; i++) {
+			int ny = y + dy[i];
+			int nx = x + dx[i];
 
-    private static void oneStep(int y, int x, int type) {
-        arr[y][x] = type;
+			// 범위 벗어나면
+			if (!outOfIdx(ny, nx))
+				continue;
+			// 해당 자리가 0이라면
+			if (arr[ny][nx] == 0)
+				continue;
+			if (arr[ny][nx] == type)
+				continue;
 
-        for (int i = 0; i < 8; i++) {
-            int ny = y;
-            int nx = x;
-            while (true) {
-                ny += dy[i];
-                nx += dx[i];
+			// 해당 자리가 다른 색깔이라면 끝까지 이동하면서 확인하기
+			ArrayList<int[]> list = new ArrayList<>();
+			boolean flag = false;
+			int nny = y;
+			int nnx = x;
+			while (true) {
+				nny += dy[i];
+				nnx += dx[i];
 
-                if (!outOfIdx(ny, nx))
-                    break; // 범위 벗어나면 종료
-                if (arr[ny][nx] == type)
-                    break; // 같은 색깔이면 종료
-                if (arr[ny][nx] == 0)
-                    break; // 빈칸이면 종료
+				if (!outOfIdx(nny, nnx))
+					break;
+				if (arr[nny][nnx] == 0)
+					break;
+				if (arr[nny][nnx] == type) {
+					flag = true;
+					break;
+				}
+				// 바꿔야 되는 돌이면
+				else if (arr[nny][nnx] != type) {
+					list.add(new int[] { nny, nnx });
+				}
+			}
 
-                // 다른 색깔이면 ny, nx 는 다른 색깔만난거
-                int nny = ny;
-                int nnx = nx;
-                Queue<int[]> q = new LinkedList<>();
-                q.add(new int[] {nny, nnx});
-                while (true) {
-                    nny += dy[i];
-                    nnx += dx[i];
-                    if (!outOfIdx(nny, nnx))
-                        break; // 범위 벗어나면 종료
-                    if (arr[nny][nnx] == 0)
-                        break; // 빈칸이면 종료
-                    if (arr[nny][nnx] == arr[ny][nx]) {
-                        q.add(new int[] { nny, nnx });
-                    } else if (arr[nny][nnx] != arr[ny][nx]) {
-                        // 다른거 만나면 끝까지 가면서 거기에 나랑같은색깔이있으면 사이에있는애들 다 바꿔주기
-                        while (!q.isEmpty()) {
-                            int[] poll = q.poll();
-                            int py = poll[0];
-                            int px = poll[1];
 
-                            arr[py][px] = arr[y][x];
-                        }
-                        break;
-                    }
-                }
-            }
-        }
-    }
+			// 해당 자리 돌 뒤집기
+			if (flag) {
+				for (int j = 0; j < list.size(); j++) {
+					int[] target = list.get(j);
+					arr[target[0]][target[1]] = type;
+				}
+			}
+		}
 
-    private static boolean outOfIdx(int ny, int nx) {
-        if (ny >= 0 && ny < n && nx >= 0 && nx < n) {
-            return true;
-        }
-        return false;
-    }
+	}
+
+	private static void checkBW() {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (arr[i][j] == 1) {
+					ansB++;
+				} else if (arr[i][j] == 2) {
+					ansW++;
+				}
+			}
+		}
+
+	}
+
+	private static boolean outOfIdx(int ny, int nx) {
+		if (ny >= 0 && ny < n && nx >= 0 && nx < n) {
+			return true;
+		}
+		return false;
+	}
+
 }
