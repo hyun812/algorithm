2f5cc9d443f5e8c1ebddfdd298261eecbc2973f1
SeungHyun <92029126+hyun812@users.noreply.github.com>
2023-12-07 01:27:04 +0900

    [D3] Title: 최장 증가 부분 수열, Time: 156 ms, Memory: 27,996 KB -BaekjoonHub

--- SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.java
+++ SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.java
@@ -1,39 +1,44 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Solution {
-	static int n;
-	static int[] arr;
-	static int[] dp;
+	static int n, k;
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
 		
 		int t = Integer.parseInt(bf.readLine());
-
-		for(int tc = 1; tc<=t; tc++) {
-			n = Integer.parseInt(bf.readLine());
+		
+		for(int tc=1; tc<=t; tc++) {
+			int n = Integer.parseInt(bf.readLine());
 			
-			arr = new int[n];
-			dp = new int[n];
-			int ans = 1;
+			int[] arr = new int[n];
+			int answer = 1;
+			
 			st = new StringTokenizer(bf.readLine());
 			for(int i=0; i<n; i++) {
+				
+				
 				arr[i] = Integer.parseInt(st.nextToken());
 			}
 			
-			dp[0] = 1;
+			int[] dp = new int[n];
+			Arrays.fill(dp, 1);
+			
 			for(int i=1; i<n; i++) {
-				dp[i] = 1;
-				
-				for(int j=i-1; j>=0; j--) {
-					if(arr[i] > arr[j]) { // 나보다 작으면
-						dp[i] = Math.max(dp[j]+1, dp[i]);
+				for(int j=i; j>=0; j--) {
+					if(arr[i] > arr[j]) {
+						dp[i] = Math.max(dp[i], dp[j]+1);
+						answer = Math.max(answer, dp[i]);
 					}
 				}
-				ans = Math.max(ans, dp[i]);
 			}
-			System.out.println("#"+tc+" "+ans);
+			
+			sb.append("#").append(tc).append(" ").append(answer).append("\n");
+			
 		}
+		System.out.println(sb.toString());
 	}
 }
