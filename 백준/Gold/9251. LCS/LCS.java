e4b6e697b7e9fe52cb9880a5b1657f03144c3f99
SeungHyun <92029126+hyun812@users.noreply.github.com>
2023-12-10 23:01:46 +0900

    [Gold V] Title: LCS, Time: 128 ms, Memory: 16352 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+//		StringTokenizer st = new StringTokenizer(bf.readLine());
+		
+		String[] s1 = bf.readLine().split("");
+		String[] s2 = bf.readLine().split("");
+		
+		int[][] dp = new int[s1.length+1][s2.length+1];
+		
+		
+		for(int i=1; i<=s1.length; i++) {
+			for(int j=1; j<=s2.length; j++) {
+				
+				if(s1[i-1].equals(s2[j-1])) {
+					dp[i][j] = dp[i-1][j-1] + 1;
+				}else {
+					dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+				}
+			}
+		}
+		System.out.println(dp[s1.length][s2.length]);
+	}
+}
