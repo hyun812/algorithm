9b83dd3f0ea19dee365c7aba250aef9618e95219
SeungHyun <92029126+hyun812@users.noreply.github.com>
2024-04-16 09:37:27 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 49.57 ms, Memory: 82 MB -BaekjoonHub

---
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.js
@@ -0,0 +1,22 @@
+
+function solution(board) {
+    let answer = 0;
+    
+    if(board.length <= 1 || board[0].length <=1){
+        return 1;
+    }
+    for(let i=1; i<board.length; i++){
+        for(let j=1; j<board[0].length; j++){
+            if(board[i][j] === 0) continue;
+            
+            const left = board[i][j-1];
+            const up = board[i-1][j];
+            const cross = board[i-1][j-1];
+            
+            board[i][j] = Math.min(left, up, cross)+1;
+            
+            answer = Math.max(answer, board[i][j]);
+        }
+    }
+    return answer*answer;
+}
