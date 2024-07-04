019ce3ee120036dae195350f6f651e71460cdcfd
SeungHyun <92029126+hyun812@users.noreply.github.com>
2024-07-04 22:36:33 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 0.36 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.js
@@ -0,0 +1,42 @@
+// 직선도로 100원, 코너 500원
+// 1이면 벽 => 갈 수 없음
+function solution(board) {
+    var answer = Infinity;
+    
+    const len = board.length;
+    const dy = [0, 1, 0, -1];
+    const dx = [1, 0, -1, 0];
+    
+    const queue = [];
+    const dp = Array(len).fill().map(() => Array(len).fill().map(() => Array(4).fill(Infinity)));
+    
+    queue.push([0, 0, 0, 0]);
+    queue.push([0, 0, 0, 1]);
+    
+    const dfs = () => {
+        
+        while(queue.length){
+            const [ y, x, cost, dir ] = queue.shift();
+            
+            for(let i=0; i<4; i++){
+                const ny = dy[i] + y;
+                const nx = dx[i] + x;
+                
+                if(ny<0 || nx<0 || ny>=len || nx>= len) continue;
+                if(board[ny][nx]) continue;
+                
+                let roadCost = dir === i ? cost+100 : cost+600;
+                
+                // 방문하지 않았거나, 비용이 더 크다면
+                if(dp[ny][nx][i] > roadCost) { 
+                    queue.push([ny, nx, roadCost, i]);
+                    dp[ny][nx][i] = roadCost;    
+                }
+            }
+        }
+    }
+    
+    dfs();
+    
+    return Math.min(...dp[len-1][len-1]);
+}
