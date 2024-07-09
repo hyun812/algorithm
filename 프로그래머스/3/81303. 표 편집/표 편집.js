2cb5e28ecadac7b69d5a4549a4fb38656ea045bd
SeungHyun <92029126+hyun812@users.noreply.github.com>
2024-07-09 16:59:25 +0900

    [level 3] Title: 표 편집, Time: 311.88 ms, Memory: 142 MB -BaekjoonHub

---
+++ 프로그래머스/3/81303. 표 편집/표 편집.js
@@ -0,0 +1,77 @@
+function Node(idx, prev) {
+    this.idx = idx;
+    this.prev = prev;
+    this.next = null;
+}
+
+function solution(n, k, cmd) {
+    let select; // 선택된 노드
+    let prevNode = new Node(0);
+    
+    for(let i=1; i<n; i++){
+        const newNode = new Node(i, prevNode);
+        prevNode.next = newNode;
+        prevNode = newNode;
+        
+        if(i===k){
+            select = newNode;
+        }
+    }
+    
+    const deleteArr = [];
+    
+    const move = (num, dir) => {
+        for(let i=0; i<num; i++){
+            if(!select[dir]) break;
+            select = select[dir];
+        }
+    }
+    
+    const deleteNode = () => {
+        const prev = select.prev;
+        const next = select.next;
+        
+        deleteArr.push(select);
+        
+        select = next ? next : prev; // 다음 노드가 있으면 next, 없으면 prev
+        
+        if(prev) prev.next = next;
+        if(next) next.prev = prev;
+    }
+    
+    const recoverNode = () => {
+        const target = deleteArr.pop();
+        
+        const prev = target.prev;
+        const next = target.next; 
+        
+        if(prev) prev.next = target;
+        if(next) next.prev = target;
+    }
+    
+    cmd.forEach((value)=>{
+        const [c, num] = value.split(" ");
+        
+        switch(c){
+            case "U":
+                move(num, "prev");
+                break;
+            case "D":
+                move(num, "next");
+                break;
+            case "C":
+                deleteNode();
+                break;
+            case "Z": 
+                recoverNode();
+                break;
+        }
+    });
+
+    const answer = Array(n).fill('O');
+    deleteArr.forEach((node) => {
+        answer[node.idx] = 'X';
+    })
+    
+    return answer.join("");
+}
