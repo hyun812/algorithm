d42570f46a97d7b313920c22da09c8b3108e7429
SeungHyun <92029126+hyun812@users.noreply.github.com>
2024-07-26 16:44:11 +0900

    [level 3] Title: 표현 가능한 이진트리, Time: 50.88 ms, Memory: 39 MB -BaekjoonHub

---
+++ 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.js
@@ -0,0 +1,34 @@
+function solution(numbers) {
+    const answer = [];
+    
+    for(let number of numbers){
+        const bi = number.toString(2);
+        const n = bi.length;
+        const m = n.toString(2).length; // 높이
+        
+        let tree = '0'.repeat(2**m -1 - n); // 포화이진트리 형태로 0을 채움
+        tree = tree + '' + bi;
+        
+        check(tree, 0, tree.length-1) ? answer.push(1) : answer.push(0);
+    }
+    
+    return answer;
+}
+
+function check (tree, start, end) {
+    const parent = Math.floor((start+end) / 2);
+    const left = Math.floor((start + parent-1)/2);
+    const right = Math.floor((parent+1 + end)/2);
+    
+    if (start == end) return true;
+    
+    //부모가 0 인데 자식이 1이라면
+    if (tree[parent] === '0' && ((tree[left] === '1') || (tree[right] === '1'))) {
+        return false;
+    }
+
+    if (!check(tree, start, parent-1)) return false;
+    if (!check(tree, parent+1, end)) return false;
+    
+    return true;
+}
