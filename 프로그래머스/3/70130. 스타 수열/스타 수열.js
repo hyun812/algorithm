3e66b3c25a5875d74a88a7f5cb8748633646e267
SeungHyun <92029126+hyun812@users.noreply.github.com>
2024-07-27 19:33:31 +0900

    [level 3] Title: 스타 수열, Time: 36.98 ms, Memory: 53.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/70130. 스타 수열/스타 수열.js
@@ -0,0 +1,23 @@
+function solution(a) {
+    let answer = 0;
+    
+    const counts = new Array(a.length).fill(0);
+    a.forEach((v) => counts[v]++ );
+    
+    for(let i=0; i<counts.length; i++){
+        if(answer >= counts[i]) continue;
+        
+        let count = 0;
+        
+        for(let j=0; j<a.length-1; j++){
+            if(a[j] === a[j+1]) continue;
+            if(a[j] !== i && a[j+1] !== i) continue;
+            
+            count++;
+            j++; // 다다음 위치로 이동
+        }
+        answer = Math.max(answer, count);
+    }
+    
+    return answer*2;
+}
