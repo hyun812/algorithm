36eefd44f0457c6b2866ae0878460da8d58bc5ab
SeungHyun <92029126+hyun812@users.noreply.github.com>
2024-07-28 17:01:33 +0900

    [level 2] Title: 요격 시스템, Time: 713.83 ms, Memory: 164 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.js
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.js
@@ -1,15 +1,14 @@
 function solution(targets) {
-    var answer = 1;
+    let answer = 0;
     
-    targets.sort((a, b) => b[0] - a[0]);
+    targets.sort((a, b) => a[1] - b[1]);
     
-    let checkPoint = targets[0][0];
-    
-    for(let i=1; i<targets.length; i++){
+    let prevEnd = -1;
+    for(let i=0; i<targets.length; i++){
         const [start, end] = targets[i];
         
-        if(end <= checkPoint) {
-            checkPoint = start;
+        if(prevEnd <= start) {
+            prevEnd = end;
             answer++;
         }
     }
