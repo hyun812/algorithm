29e0cd0a781e6b06587d1034b4264172c167a7a0
SeungHyun <92029126+hyun812@users.noreply.github.com>
2024-07-29 21:54:47 +0900

    [level 2] Title: 순위 검색, Time: 830.92 ms, Memory: 111 MB -BaekjoonHub

---
+++ 프로그래머스/2/72412. 순위 검색/순위 검색.js
@@ -0,0 +1,47 @@
+function solution(info, query) {
+    var answer = [];
+    
+    const users = {};
+    info.map((v)=>{
+        const arr = v.split(" ");
+        const score = Number(arr.pop());
+        
+        const key = arr.join("");
+    
+        users[key] ? users[key].push(score) : users[key] = [score];
+    });
+    Object.keys(users).forEach(key => users[key].sort((a, b) => a - b));
+
+    query
+        .map(q => q.split(/ and | |-/i) // ' and ' 또는 " " 또는 "-"로 split
+        .filter(e=>e)) // 유효한 값만
+        .forEach((q) => {
+            const score = q.pop();
+            const result = getResult(users, q, score);
+            answer.push(result);
+        })
+    
+    return answer;
+}
+
+function getResult(users, query, score) {
+    const key = Object.keys(users);
+    return key
+            .filter(k => query.every(v => k.includes(v)))
+            .reduce((acc, cur) => acc + binarySearch(users[cur], score), 0);
+            // .reduce((acc, cur) => acc + users[cur].length - binarySearch(users[cur], score), 0);
+}
+
+function binarySearch(arr, target) {
+    let left = 0;
+    let right = arr.length;
+    let result = 0;
+    
+    while(left < right) {
+        const mid = Math.floor((left + right) / 2);
+        
+        if (arr[mid] >= target) right = mid;
+        else if (arr[mid] < target) left = mid + 1;
+    }
+    return arr.length -left; 
+}
