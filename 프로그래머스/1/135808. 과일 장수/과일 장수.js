68e9160c7827951273e6a5d62406f0fffc7ac943
SeungHyun <92029126+hyun812@users.noreply.github.com>
2024-08-14 17:35:14 +0900

    [level 1] Title: 과일 장수, Time: 230.75 ms, Memory: 93.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -0,0 +1,13 @@
+function solution(k, m, score) {
+    var answer = 0;
+    
+    score.sort((a, b) => b-a);
+    
+    for(let i=0; i<score.length; i+=m){
+        if(score.length-i < m) break;
+        answer += score.slice(i, i+m)[m-1] * m;
+    }
+
+    
+    return answer;
+}
