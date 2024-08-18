09c26109fc47fd55207021eb356323d0eb91a5c2
SeungHyun <92029126+hyun812@users.noreply.github.com>
2024-08-18 20:36:15 +0900

    [level 1] Title: 공원 산책, Time: 0.49 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.js
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.js
@@ -2,46 +2,20 @@ function solution(park, routes) {
     let [y, x] = [-1, -1];
     
     for(let i=0; i<park.length; i++){
-        for(let j=0; j<park[0].length; j++){
-            if(park[i][j] === 'S'){
-                y = i;
-                x = j;
-            }
+        if(park[i].includes('S')) {
+            [y, x] = [i, park[i].indexOf('S')];    
         }
-        if(y !== -1 && x !== -1) break;
     }
-    
-    const dirToIndex = (dir) => {
-        let index = -1;
-        switch(dir) {
-            case "N" :
-                index = 0;
-                break;
-            case "S" :
-                index = 1;
-                break;
-            case "W" :
-                index = 2;
-                break;
-            case "E" :
-                index = 3;
-                break;
-        }
-        return index;
-    }
-
-    const dy = [-1, 1, 0, 0];
-    const dx = [0, 0, -1, 1]
+    const dirs = { N: [-1, 0], S: [1, 0], W: [0, -1], E: [0, 1]};
     routes.forEach((route) => {
-        const [dir, count] = route.split(" ");
+        const [r, n] = route.split(" ");
         
-        const idx = dirToIndex(dir);
         let [copyY, copyX] = [y, x];
         let isOK = true;
         
-        for(let i=0; i<count; i++){
-            const ny = y+dy[idx];
-            const nx = x+dx[idx];
+        for(let i=0; i<n; i++){
+            const ny = y+dirs[r][0];
+            const nx = x+dirs[r][1];
             
             if(ny < 0 || nx < 0 || ny >= park.length || nx >= park[0].length || park[ny][nx] === "X") {
                 isOK = false;
