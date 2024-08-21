f76b9ac0aaa8df2206675f77c961989bbf50dfa8
SeungHyun <92029126+hyun812@users.noreply.github.com>
2024-08-21 17:34:53 +0900

    [level 4] Title: 올바른 괄호의 갯수, Time: 392.30 ms, Memory: 35.3 MB -BaekjoonHub

--- 프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.js
+++ 프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.js
@@ -1,12 +1,20 @@
 function solution (n) {
-    const dp = new Array(n+1).fill(0);
-    dp[0] = dp[1] = 1;
+    let answer = 0;
     
-    for(let i = 2; i <= n; i++) {
-        for(let j = 1; j <= i; j++) {
-            dp[i] += dp[j-1] * dp[i-j];
+    const dfs = (open, close) => {
+        if(open+close === n*2){
+            if(open === n && close === n) answer++;
+            return;
         }
+        
+        if(open > close){
+            dfs(open, close+1);
+        }
+        dfs(open+1, close);
+        return;
     }
+    
+    dfs(0, 0);
   
-    return dp[n]; 
+    return answer; 
 }
