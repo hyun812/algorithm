538e7b431495ce5b89b9c981ef5c629b8f0b7317
SeungHyun <dltmdgus4802@gmail.com>
2024-08-29 17:11:16 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 104 ms, Memory: 9388 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.js
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.js
@@ -1,3 +1,37 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.front = null;
+    this.rear = null;
+    this.size = 0;
+  }
+
+  enqueue(data) {
+    const newNode = new Node(data);
+
+    if (!this.size) this.front = newNode;
+    else this.rear.next = newNode;
+
+    this.rear = newNode;
+    this.size++;
+  }
+
+  dequeue() {
+    if (!this.size) return null;
+
+    const data = this.front.data;
+    this.front = this.front.next;
+    this.size--;
+    return data;
+  }
+}
+
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? './dev/stdin' : 'index.txt';
 const input = fs.readFileSync(filePath).toString().trim().split('\n');
@@ -13,11 +47,12 @@ for (let i = 1; i <= N + M; i++) {
 
 const bfs = () => {
   const visited = new Array(101).fill(0);
-  const queue = [[1, 0]];
+  const queue = new Queue();
+  queue.enqueue([1, 0]);
   visited[0] = 1;
 
-  while (queue.length) {
-    const [cur, cnt] = queue.shift();
+  while (queue.size) {
+    const [cur, cnt] = queue.dequeue();
 
     if (cur === 100) {
       return cnt;
@@ -29,7 +64,7 @@ const bfs = () => {
       if (next >= 101) continue;
       if (visited[next]) continue;
 
-      queue.push([map[next], cnt + 1]);
+      queue.enqueue([map[next], cnt + 1]);
       visited[next] = 1;
     }
   }
