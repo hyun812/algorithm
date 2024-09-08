a84946e617489162804a869d0f48ebe7f3810a93
SeungHyun <dltmdgus4802@gmail.com>
2024-09-08 15:50:04 +0900

    [level 3] Title: 입국심사, Time: 132.47 ms, Memory: 43.5 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.js
+++ 프로그래머스/3/43238. 입국심사/입국심사.js
@@ -1,24 +1,21 @@
-// n : 입국심사를 기다리는 사람 수
-// times: 각 심사관이 한명을 심사하는데 걸리는 시간
 function solution(n, times) {
+    times.sort((a, b) => a - b);
     
-    times.sort((a,b) => a-b); // 오름차순 정렬
-    
-    let left = 1;
-    let right = times[times.length-1] * n; // 최대로 걸릴 수 있는 시간
-    
-    while(left <= right){
-        const mid = Math.floor((left+right)/2);
+    let left = 0;
+    let right = times[times.length - 1] * n;
+    let answer = times[times.length - 1] * n;
+    while(left <= right) {
+        const mid = Math.floor((left + right) / 2);
         
-        // 처리할 수 있는 최대 입국자 수 계산
-        const sum = times.reduce((acc, cur) => acc + Math.floor(mid / cur), 0);
+        const count = times.reduce((acc, cur) => acc + Math.floor(mid / cur), 0);
         
-        if(sum < n){ // n보다 작다면 시간 증가
-            left = mid+1;
-        }else { // n보다 크거나 같다면 시간 감소
-            right = mid-1;    
+        if(count < n) {
+            left = mid + 1;
+        }else {
+            right = mid - 1;
+            answer = Math.min(answer, mid);
         }
     }
     
-    return left;
+    return answer;
 }
