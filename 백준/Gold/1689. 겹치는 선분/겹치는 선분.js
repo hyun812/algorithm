e2d944b548801dea8b7c0d71a764e944a0db18cd
SeungHyun <dltmdgus4802@gmail.com>
2024-10-29 17:09:11 +0900

    [Gold IV] Title: 겹치는 선분, Time: 3964 ms, Memory: 387724 KB -BaekjoonHub

---
+++ 백준/Gold/1689. 겹치는 선분/겹치는 선분.js
@@ -0,0 +1,21 @@
+const path = process.platform === 'linux' ? '/dev/stdin' : 'index.txt';
+const input = require('fs').readFileSync(path).toString().trim().split('\n');
+
+const N = +input[0];
+const arr = [];
+
+for (let i = 0; i < N; i++) {
+  const [start, end] = input[i + 1].split(' ').map(Number);
+  arr.push([start, 1]);
+  arr.push([end, -1]);
+}
+
+arr.sort((a, b) => (a[0] === b[0] ? a[1] - b[1] : a[0] - b[0]));
+
+let answer = 0;
+let sum = 0;
+for (const [_, num] of arr) {
+  sum += num;
+  answer = Math.max(answer, sum);
+}
+console.log(answer);
