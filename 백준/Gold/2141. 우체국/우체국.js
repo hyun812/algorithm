a563fa165a68e19b1d41c14f0dbf23859b02d257
SeungHyun <dltmdgus4802@gmail.com>
2024-10-29 16:34:26 +0900

    [Gold IV] Title: 우체국, Time: 380 ms, Memory: 47576 KB -BaekjoonHub

---
+++ 백준/Gold/2141. 우체국/우체국.js
@@ -0,0 +1,21 @@
+const path = process.platform === 'linux' ? '/dev/stdin' : 'index.txt';
+const input = require('fs').readFileSync(path).toString().trim().split('\n');
+
+const [N, ...arr] = input;
+
+const city = arr.map((i) => i.split(' ').map(Number));
+city.sort((a, b) => a[0] - b[0]);
+
+const peopleCount = city.reduce((acc, cur) => acc + cur[1], 0);
+
+let sum = 0;
+let answer = -1;
+for (let i = 0; i < N; i++) {
+  sum += city[i][1];
+  if (peopleCount / 2 <= sum) {
+    answer = city[i][0];
+    break;
+  }
+}
+
+console.log(answer);
