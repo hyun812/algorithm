3ee5bcd913f2e21ba31f52355478ec71b6d1a8ca
SeungHyun <dltmdgus4802@gmail.com>
2024-11-27 18:33:00 +0900

    [Gold III] Title: 크게 만들기, Time: 208 ms, Memory: 34412 KB -BaekjoonHub

--- 백준/Gold/2812. 크게 만들기/크게 만들기.js
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.js
@@ -2,22 +2,18 @@ const path = process.platform === 'linux' ? '/dev/stdin' : 'index.txt';
 const input = require('fs').readFileSync(path).toString().trim().split('\n');
 
 let [N, K] = input[0].split(' ').map(Number);
-const num = input[1].split('').map(Number);
+const numbers = input[1].split('').map(Number);
 
 const stack = [];
-
 for (let i = 0; i < N; i++) {
-  const cur = num[i];
-
-  while (stack.length && stack[stack.length - 1] < cur && K) {
-    stack.pop();
+  while (stack.length && stack[stack.length - 1] < numbers[i] && K) {
     K--;
+    stack.pop();
   }
-
-  stack.push(cur);
+  stack.push(numbers[i]);
 }
 
-for (let i = 0; i < K; i++) {
+while (K--) {
   stack.pop();
 }
 
