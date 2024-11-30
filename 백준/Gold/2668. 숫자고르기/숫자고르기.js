85b7287df84dc86cb35202378c07a31f9ba2b202
SeungHyun <dltmdgus4802@gmail.com>
2024-11-30 18:32:42 +0900

    [Gold V] Title: 숫자고르기, Time: 92 ms, Memory: 9348 KB -BaekjoonHub

--- 백준/Gold/2668. 숫자고르기/숫자고르기.js
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.js
@@ -1,14 +1,14 @@
-const fs = require('fs');
-const filePath = process.platform === 'linux' ? './dev/stdin' : 'index.txt';
-const input = fs.readFileSync(filePath).toString().trim().split('\n');
+const path = process.platform === 'linux' ? '/dev/stdin' : 'index.txt';
+const input = require('fs').readFileSync(path).toString().trim().split('\n');
 
-const N = +input.shift();
-const num = [0, ...input.map(Number)];
+const N = +input[0];
+const arr = [0, ...input.slice(1).map(Number)];
 const visited = Array(N + 1).fill(0);
+
 const answer = [];
 
 const dfs = (start, target) => {
-  const next = num[start];
+  const next = arr[start];
 
   if (!visited[next]) {
     visited[next] = 1;
