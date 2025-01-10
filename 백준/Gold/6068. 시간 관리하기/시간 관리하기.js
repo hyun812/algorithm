ec8736a11db2c8d6da3d9ccf9e6a61fdcaef21e5
SeungHyun <dltmdgus4802@gmail.com>
2025-01-10 18:11:21 +0900

    [Gold V] Title: 시간 관리하기, Time: 96 ms, Memory: 9728 KB -BaekjoonHub

--- 백준/Gold/6068. 시간 관리하기/시간 관리하기.js
+++ 백준/Gold/6068. 시간 관리하기/시간 관리하기.js
@@ -6,14 +6,13 @@ const N = +input[0];
 const works = input
   .slice(1)
   .map((lines) => lines.split(' ').map(Number))
-  .map(([t, s]) => [t, s, s - t])
   .sort((a, b) => b[1] - a[1]);
 
-let answer = works[0][2];
+let answer = works[0][1] - works[0][0];
 for (let i = 1; i < N; i++) {
-  const [t, s, minStartT] = works[i];
+  const [t, s] = works[i];
 
-  answer = Math.min(answer - t, minStartT);
+  answer = Math.min(answer - t, s - t);
 }
 
 answer < 0 ? console.log(-1) : console.log(answer);
