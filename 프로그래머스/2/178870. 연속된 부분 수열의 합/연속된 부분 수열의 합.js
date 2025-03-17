6a40b2c2481287f47c7018e9ffcdcbc376553cb3
SeungHyun <dltmdgus4802@gmail.com>
2025-03-17 16:57:59 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 16.91 ms, Memory: 65.8 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.js
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.js
@@ -1,40 +1,32 @@
-/*
-    k : 부분수열의 합
-    -> 여러개인 경우 길이가 짧은 수열
-    -> 그것도 여러개라면 인덱스가 작은 수열
-    
-    result - 시작인덱스 마지막 인덱스
-*/
 function solution(sequence, k) {
-    
-    const answer = [];
+    const answer = [0, Infinity];
     
     let left = 0;
     let right = 0;
-    let sum = sequence[0];
-    let len = Infinity;
-    // 작으면 right ++ , 크면 left++
     
-    while(right < sequence.length){
-        
-        if(sum == k){
-            if(len > (right-left+1)){
-                len = right-left+1;
+    let sum = sequence[0];
+    
+    while (left <= right && right < sequence.length) {
+        if (sum === k) {
+            if (answer[1] - answer[0] > right - left) {
+                answer[0] = left;
+                answer[1] = right;
+            }else if (answer[1] - answer[0] === right - left && left < answer[0]) {
                 answer[0] = left;
                 answer[1] = right;
             }
             right++;
             sum += sequence[right];
-        }else if(sum < k){
+            sum -= sequence[left];
+            left++;
+        }else if (sum < k) {
             right++;
             sum += sequence[right];
-        }else if(sum > k){
+        }else {
             sum -= sequence[left];
             left++;
         }
     }
-    
-    
     
     return answer;
 }
