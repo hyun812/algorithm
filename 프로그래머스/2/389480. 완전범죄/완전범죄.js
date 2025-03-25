8034a7de691dbefdaf925274c4fae9d0f3cdd711
SeungHyun <dltmdgus4802@gmail.com>
2025-03-25 19:18:30 +0900

    [level 2] Title: 완전범죄, Time: 0.79 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.js
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.js
@@ -1,41 +1,29 @@
 function solution(info, n, m) {
-  const INF = Infinity;
+    const len = info.length;
+    const dp = Array.from({ length: len + 1 }, () => Array(m).fill(Infinity));
 
-  // A의 누적 흔적을 인덱스로 사용하여, B의 최소 누적 흔적을 저장
-  let dp = Array(n).fill(INF);
-  dp[0] = 0; // A 흔적이 0일 때, B도 0으로 시작
+    dp[0][0] = 0;
+    
+    for (let i = 0; i < len; i++) {
+        const [aTrace, bTrace] = info[i];        
+        
+        for (let j = m - 1; j >= 0; j--) {
+            if (dp[i][j] === Infinity) continue;
+            
+            
+            // A가 훔치는 경우
+            let newA = dp[i][j] + aTrace;
+            let newB = j;
+            if (newA < n) dp[i + 1][newB] = Math.min(dp[i + 1][newB], newA);
 
-  for (const [aTrace, bTrace] of info) {
-    const nextDp = Array(n).fill(INF);
-
-    for (let a = 0; a < n; a++) {
-      const b = dp[a];
-      if (b === INF) continue;
-
-      // A가 훔치는 경우
-      const newA = a + aTrace;
-      const newB = b;
-      if (newA < n) {
-        nextDp[newA] = Math.min(nextDp[newA], newB);
-      }
-
-      // B가 훔치는 경우
-      const newA2 = a;
-      const newB2 = b + bTrace;
-      if (newB2 < m) {
-        nextDp[newA2] = Math.min(nextDp[newA2], newB2);
-      }
+            // B가 훔치는 경우
+            let newA2 = dp[i][j];
+            let newB2 = j + bTrace;
+            if (newB2 < m) dp[i + 1][newB2] = Math.min(dp[i + 1][newB2], newA2);
+        }
     }
 
-    dp = nextDp;
-  }
+    const result = Math.min(...dp[len]);
 
-  // A가 경찰에 안 잡히고, B도 안 잡히는 최소 A 흔적 찾기
-  for (let a = 0; a < n; a++) {
-    if (dp[a] < m) {
-      return a;
-    }
-  }
-
-  return -1; // 불가능한 경우
+    return result === Infinity ? -1 : result
 }
