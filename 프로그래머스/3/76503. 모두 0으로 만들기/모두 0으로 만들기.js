b9f88323e150098c0160c19e5b5be28153a96a37
SeungHyun <dltmdgus4802@gmail.com>
2025-04-11 17:00:13 +0900

    [level 3] Title: 모두 0으로 만들기, Time: 590.64 ms, Memory: 209 MB -BaekjoonHub

---
+++ 프로그래머스/3/76503. 모두 0으로 만들기/모두 0으로 만들기.js
@@ -0,0 +1,34 @@
+function solution(a, edges) {
+    if (a.reduce(((acc, cur) => acc + cur), 0) !== 0) return -1
+    
+    const graph = Array.from({ length: a.length }, () => []);
+    for (const [from, to] of edges) {
+        graph[from].push(to);
+        graph[to].push(from);    
+    }
+    
+    let answer = 0n;
+    const visited = new Array(a.length).fill(0);
+    const stack = [ [0, -1] ];     
+    
+    while (stack.length) {
+        const [cur, parent] = stack.pop();
+        
+        if (visited[cur]) {
+            answer += BigInt(Math.abs(a[cur]));
+            a[parent] += a[cur];
+            a[cur] = 0;
+            continue;    
+        }
+        
+        stack.push([cur, parent]);
+        visited[cur] = 1;
+        
+        for (const next of graph[cur]) {
+            if (visited[next]) continue;
+            stack.push([next, cur]);
+        }
+    }
+    console.log(a)
+    return a[0] ? -1 : answer;
+}
