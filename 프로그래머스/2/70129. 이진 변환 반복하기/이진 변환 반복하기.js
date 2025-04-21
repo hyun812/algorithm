67819db8876dd8f921deb577582f994b9a8c18a6
SeungHyun <dltmdgus4802@gmail.com>
2025-04-21 19:26:30 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 12.07 ms, Memory: 36.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -0,0 +1,16 @@
+function solution(s) {
+    var answer = [0, 0];
+    
+    while (true) {
+        if (s.length === 1) break;
+        const re = s.replaceAll('0', '');
+        answer[0]++;
+        answer[1] += s.length - re.length;
+        
+        s = re.length.toString(2)
+        console.log(s);
+    }
+    
+    
+    return answer;
+}
