333dd0c7850fdabc9e24a3cdd47a134e0a640a65
SeungHyun <dltmdgus4802@gmail.com>
2025-07-08 10:37:25 +0900

    [level 2] Title: 튜플, Time: 37.94 ms, Memory: 42.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/64065. 튜플/튜플.js
@@ -0,0 +1,13 @@
+function solution(s) {
+    const answer = [];
+    const arr = JSON.parse(s.replace(/{/g, "[").replace(/}/g, "]"));
+    
+    arr.sort((a, b) => a.length - b.length);
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+            if (!answer.includes(arr[i][j])) answer.push(arr[i][j]);
+        }
+    }
+    
+    return answer
+}
