e6f534a2fb74c278a095defe72e32f0635d20142
SeungHyun <dltmdgus4802@gmail.com>
2025-07-12 21:42:19 +0900

    [level 2] Title: 롤케이크 자르기, Time: 119.55 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.js
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.js
@@ -1,38 +1,27 @@
-/*
-    개수와 상관없이 동일한 가짓수의 토핑이 올라가면 공평함
-    
-*/
 function solution(topping) {
     let answer = 0;
+    const len = topping.length;
     
-    let [lCnt, rCnt] = [1, 0];
-    let lArr = Array(topping.length).fill(0);
-    let rArr = Array(topping.length).fill(0);
+    const a = new Map();
+    const b = new Map();
     
-    lArr[topping[0]] += 1;
-    for(let i=1; i<topping.length; i++){
-        if(!rArr[topping[i]]) rCnt += 1;
-        rArr[topping[i]] += 1;
-    }
-    // i -> 현재 타겟
-    for(let i=1; i<topping.length; i++){
-        lArr[topping[i]] += 1;
-        rArr[topping[i]] -= 1;
-
-        
-        if(lArr[topping[i]] === 1){
-            lCnt++;    
-        }
-        
-        if(rArr[topping[i]] === 0){
-            rCnt--;
-        }
-        
-        
-        if(lCnt === rCnt) answer++;
+    a.set(topping[0], 1);
+    for (let i = 1; i < len; i++) {
+        b.set(topping[i], (b.get(topping[i]) || 0) + 1);
     }
     
+    if (a.size === b.size) answer++;
     
-    
+    for (let i = 1; i < len - 1; i++) {
+        a.set(topping[i], (a.get(topping[i]) || 0) + 1);
+        
+        if (b.get(topping[i]) === 1) {
+            b.delete(topping[i]);
+        }else {
+            b.set(topping[i], (b.get(topping[i])) - 1);    
+        }
+        
+        if (a.size === b.size) answer++;
+    }
     return answer;
 }
