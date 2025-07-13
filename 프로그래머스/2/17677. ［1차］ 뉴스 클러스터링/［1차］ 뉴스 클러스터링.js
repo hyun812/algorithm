2e40ab6e0a5808a2f38a91099256864e1fabd700
SeungHyun <dltmdgus4802@gmail.com>
2025-07-13 19:48:35 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
@@ -3,50 +3,33 @@
 // 원소의 중복을 허용함 (교집합은 min , 합집합은 max)
 function solution(str1, str2) {
     let answer = 0;
-    const a = new Map();
-    const b = new Map();
     
-    str1 = str1.toLowerCase();
-    str2 = str2.toLowerCase();
-
-    for (let i = 0; i < str1.length - 1; i++) {
-        const str = str1[i] + str1[i+1];
-        if (isEnglish(str)) a.set(str, (a.get(str) || 0) + 1);
-    }
-    
-    for (let i = 0; i < str2.length - 1; i++) {
-        const str = str2[i] + str2[i+1];
-        if (isEnglish(str)) b.set(str, (b.get(str) || 0) + 1);
-    }
-    
-    if (!a.size && !b.size) return 65536;
-    
-    // 교집합
-    let intersection = 0;
-    for (const [key, value] of a) {
-        if (!b.has(key)) continue;
-        intersection += Math.min(value, b.get(key));
-    }
-    
-    // 합집합
-    let union = 0;
-    const visited = new Set();
-    for (const [key, value] of a) {
-        if (b.has(key)) {
-            union += Math.max(value, b.get(key));
-            visited.add(key);
-        }else {
-            union += value;
+    const makeArr = (str) => {
+        const results = [];
+        for (let i = 0; i < str.length - 1; i++) {
+            const item = str[i] + str[i+1];
+            if (item.match(/^[a-zA-Z]+$/)) {
+                results.push(item.toLowerCase());
+            }
         }
-    }
-    for (const [key, value] of b) {
-        if (visited.has(key)) continue;
-        union += value;
+        return results;
     }
     
+    const arr1 = makeArr(str1);
+    const arr2 = makeArr(str2);
+    const set = new Set([...arr1, ...arr2]);
+    
+    let intersection = 0;
+    let union = 0;
+    
+    set.forEach((item) => {
+        const has1 = arr1.filter(v => v === item).length;
+        const has2 = arr2.filter(v => v === item).length;
+        
+        intersection += Math.min(has1, has2);
+        union += Math.max(has1, has2);
+    })
+    
+    if (!intersection && !union) return 65536;
     return Math.floor(intersection / union * 65536);
-}
-
-function isEnglish(str) {
-  return /^[a-zA-Z]+$/.test(str);
 }
