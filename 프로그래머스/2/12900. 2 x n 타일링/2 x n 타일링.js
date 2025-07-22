6c51a7143a426f3e884ec770fd922bd2b31a6ead
SeungHyun <dltmdgus4802@gmail.com>
2025-07-22 14:54:59 +0900

    [level 2] Title: 2 x n 타일링, Time: 2.31 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.js
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.js
@@ -1,10 +1,11 @@
 function solution(n) {
+    const dp = Array(n + 1).fill(0);
     
-    const dp = [1, 1];
-        
-    for(let i=2; i<=n; i++){
-        dp[i] = (dp[i-1] + dp[i-2]) % 1000000007;
+    dp[1] = 1;
+    dp[2] = 2;
+    
+    for (let i = 3; i <= n; i++) {
+        dp[i] = (dp[i-2] + dp[i-1]) % 1000000007;
     }
-    
     return dp[n];
 }
