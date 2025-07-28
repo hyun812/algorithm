41b0b72323b96043361b75532468a56d9e774533
SeungHyun <dltmdgus4802@gmail.com>
2025-07-28 17:40:37 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/README.md
+++ 프로그래머스/2/148653. 마법의 엘리베이터/README.md
@@ -1,10 +1,10 @@
 # [level 2] 마법의 엘리베이터 - 148653 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/148653#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/148653) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.07 ms
+메모리: 33.4 MB, 시간: 0.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 12일 10:07:43
+2025년 07월 28일 17:40:33
 
 ### 문제 설명
 
--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.js
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.js
@@ -1,42 +1,22 @@
-/*
-    더한 값이 0보다 작으면 움직이지 않는다
-    -1, +1, -10, +10, -100, +100
-*/
-
 function solution(storey) {
     let answer = 0;
     
-    const arr = [-1, 1, -10, 10, -100, 100];
-    const dp = [];
-    
-    // 만약 자리가 6,7,8,9 -> 더하기
-    // 만약 자리가 1,2,3,4,5 -> 빼기
-    
-    let temp = 0;
-    
-    for(let i=storey.toString().length-1; i>=0; i--){
+    while (storey) {
+        let cur = storey % 10;
+        let next = (storey / 10) % 10;
         
-        let target = Number(storey.toString().split('')[i]) + temp;
-        
-        if(target > 5){
-            answer += 10 - target;
-            temp = 1;        
-        }else if(target == 5){
-            // 5인경우 앞자리까지 확인해주기
-            const prev = i-1 >= 0 ? Number(storey.toString().split('')[i-1]) : 0;
-            // console.log(prev);
-            if(prev >= 5) {
-                answer += 10 - target;
-                temp = 1;        
-            }else{
-                answer += target;
-                temp = 0;    
-            }
-        }else{
-            answer += target;
-            temp = 0;
+        if (cur > 5) {
+            answer += 10 - cur;
+            storey += 10;
+        } else if (cur === 5) {
+            answer += 5;
+            storey += next >= 5 ? 10 : 0;
+        } else {
+            answer += cur;
         }
+        
+        storey = Math.floor(storey / 10);
     }
     
-    return temp === 1 ? answer+1 : answer;
+    return answer;
 }
