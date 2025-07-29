29d498570e957bf96a8a09239fbdde8122d8d482
SeungHyun <dltmdgus4802@gmail.com>
2025-07-29 09:14:11 +0900

    [level 2] Title: 시소 짝꿍, Time: 55.56 ms, Memory: 43.7 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.js
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.js
@@ -1,20 +1,18 @@
 function solution(weights) {
     let answer = 0;
     
-    const ratio = [1, 3/2, 2, 4/3];
     const map = new Map();
+    const ratio = [1, 3 / 2, 2, 4 / 3];
     
-    weights.sort((a, b) => b-a).forEach((w)=>{
-        
-        ratio.forEach((r) =>{
-            if(map.has(w*r)) {
+    weights.sort((a, b) => b - a);
+    weights.forEach((w) => {
+        ratio.forEach(r => {
+            if (map.has(w * r)) {
                 answer += map.get(w * r);
-            }
+            }  
         })
-        
-        map.set(w, (map.get(w) || 0) +1);
+        map.set(w, (map.get(w) || 0) + 1);
     })
-    
     
     return answer;
 }
