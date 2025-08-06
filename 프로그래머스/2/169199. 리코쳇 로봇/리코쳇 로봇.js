5b5892f32d8330c386df9fafa9e00a82e72c5a55
SeungHyun <dltmdgus4802@gmail.com>
2025-08-06 12:47:18 +0900

    [level 2] Title: 리코쳇 로봇, Time: 5.25 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.js
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.js
@@ -1,70 +1,61 @@
-// 장애물이나 맨끝에 부딪힐 때까지 이동
-
 function solution(board) {
-    let answer = 0;
-    
-    let [startY, startX] = [0, 0];
+    const N = board.length;
+    const M = board[0].length;
     
     const dy = [-1, 1, 0, 0];
     const dx = [0, 0, -1, 1];
     
-    const visited = Array.from({length: board.length}, ()=>Array(board[0].length).fill(0));
+    let [startY, startX] = [0, 0]
+    let [endY, endX] = [0, 0]
     
-    for(let i=0; i<board.length; i++){
-        for(let j=0; j<board[0].length; j++){
-            if(board[i][j] !== "R") continue;
-            
-            startY = i;
-            startX = j;
-            break;
+    for (let i = 0; i < N; i++) {
+        for (let j = 0; j < M; j++) {
+            if (board[i][j] === 'R') [startY, startX] = [i, j];
+            else if (board[i][j] === 'G') [endY, endX] = [i, j];
         }
     }
-        
-    const doit = () => {
-        
-        const queue = [ [startY, startX, 0] ];
-        
-        while(queue.length){
-            const [y, x, cnt] = queue.shift();
+    
+    const moveRobot = (y, x, dir) => {
+        let ny = y;
+        let nx = x;
+        while (true) {
+            let moveY = ny + dy[dir];
+            let moveX = nx + dx[dir];
+            if (moveY < 0 || moveX < 0 || moveY >= N || moveX >= M) break;
+            if (board[moveY][moveX] === 'D') break;
             
-            // 목표 지점이라면 리턴
-            if(board[y][x] === 'G'){
-                return cnt;
+            ny = moveY;
+            nx = moveX;
+        }
+        return [ny, nx];
+    }
+    
+    const bfs = () => {
+        const queue = [];
+        const visited = Array.from({ length: N }, () => Array(M).fill(0));
+        
+        visited[startY][startX] = 1;
+        queue.push([startY, startX, 0]);
+        
+        while (queue.length) {
+            const [y, x, count] = queue.shift();
+            
+            if (y === endY && x === endX) {
+                return count;
             }
-
-            for(let i=0; i<4; i++){
-                let ny = y;
-                let nx = x;
+            
+            for (let i = 0; i < 4; i++) {
+                const [ny, nx] = moveRobot(y, x, i);
                 
-                // 미끄러져서 이동
-                while(true){
-                    // 범위를 벗어낫거나
-                    if(!outOfIdx(ny+dy[i], nx+dx[i])) break;
-                    // 장애물이거나
-                    if(board[ny+dy[i]][nx+dx[i]] === 'D') break;
-                    
-                    ny += dy[i];
-                    nx += dx[i];
-                }
-                
-                // 방문했던적이 있으면
-                if(visited[ny][nx]) continue;
-                
+                if (visited[ny][nx]) continue;
                 visited[ny][nx] = 1;
-                queue.push([ny, nx, cnt+1]);
+                queue.push([ny, nx, count + 1]);
             }
         }
+        
+        return -1;
     }
     
-    const outOfIdx = (ny, nx) => {
-        if(ny >= 0 && ny<board.length && nx>=0 && nx<board[0].length){
-            return true;
-        }
-        return false;
-    }
     
-    answer = doit();
-    
-    
-    return answer ? answer : -1;
+    return bfs();
 }
