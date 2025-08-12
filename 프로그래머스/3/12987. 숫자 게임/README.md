1e5860620a4a72bcd13f1d1426ed54a07ea4c17f
SeungHyun <dltmdgus4802@gmail.com>
2025-08-12 17:09:38 +0900

    [level 3] Title: 숫자 게임, Time: 74.21 ms, Memory: 45.5 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/README.md
+++ 프로그래머스/3/12987. 숫자 게임/README.md
@@ -1,10 +1,10 @@
 # [level 3] 숫자 게임 - 12987 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12987) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12987#qna) 
 
 ### 성능 요약
 
-메모리: 45.2 MB, 시간: 62.38 ms
+메모리: 45.5 MB, 시간: 74.21 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 01일 17:34:44
+2025년 08월 12일 17:09:37
 
 ### 문제 설명
 
--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.js
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.js
@@ -1,17 +1,22 @@
 function solution(A, B) {
     let answer = 0;
     
-    A.sort((a, b) => b-a);
-    B.sort((a, b) => a-b);
+    A.sort((a, b) => a - b);
+    B.sort((a, b) => a - b);
     
-    for(let i=0; i<A.length; i++){
-        const max = B[B.length - 1];
-        
-        if(A[i] < max){
-            answer++;
-            B.pop();
+    let ap = 0;
+    let bp = 0;
+    
+    while (ap < A.length && bp < A.length) {
+        if (A[ap] < B[bp]) {
+            answer++
+            ap++;
+            bp++;
+        } else {
+            bp++;
         }
     }
+    
     
     return answer;
 }
