41737a5993236ecf41abfe833a9e2619bdb2f4fd
SeungHyun <dltmdgus4802@gmail.com>
2025-08-13 18:00:32 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.04 ms, Memory: 33 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -0,0 +1,9 @@
+function solution(s) {
+    const len = s.length;
+    
+    if (len % 2 === 0) {
+        return `${s[Math.floor(len / 2) - 1]}${s[Math.floor(len / 2)]}`;
+    } else {
+        return s[Math.floor(len / 2)];
+    }
+}
