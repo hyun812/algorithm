460dc1cbdb12deca2d4bfe847e03b771d77025e5
SeungHyun <dltmdgus4802@gmail.com>
2025-08-14 10:34:26 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,13 @@
+function solution(num) {
+    let answer = 0;
+    
+    while (answer < 500 && num !== 1) {
+        if (num % 2 === 0) {
+            num = num / 2;
+        } else {
+            num = num * 3 + 1;
+        } 
+        answer++;
+    }
+    return answer === 500 ? -1 : answer;
+}
