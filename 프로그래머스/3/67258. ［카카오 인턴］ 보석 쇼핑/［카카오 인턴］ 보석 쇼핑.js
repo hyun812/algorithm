33db2083dc4f278faef10dce8ea0d2100de683e7
SeungHyun <dltmdgus4802@gmail.com>
2025-08-19 12:43:15 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 16.24 ms, Memory: 40.1 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.js
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.js
@@ -1,28 +1,26 @@
 function solution(gems) {
-    let answer = [1, gems.length];
-    const cnt = new Set(gems).size;
+    const answer = [1, gems.length];
+    const gemCount = new Set(gems).size;
     
-    let l = 0, r = 0;
-    const hit = new Map();
+    let left = 0;
+    let right = 0;
+    const map = new Map();
+    map.set(gems[0], 1);
     
-    hit.set(gems[0], 1)
-    
-    while(r < gems.length){
-        if(hit.size === cnt){ // 모든 보석이 포함되어 있다면 왼쪽을 늘려 나가면서 확인
-            if(answer[1] - answer[0] > r-l)
-                answer = [l+1, r+1];
+    while (right < gems.length) {
+        if (map.size === gemCount) {
+            if (answer[1] - answer[0] > right - left) {
+                answer[0] = left + 1;
+                answer[1] = right + 1;
+            }
+            map.set(gems[left], map.get(gems[left]) - 1);
+            if (map.get(gems[left]) === 0) map.delete(gems[left]);
+            left++;
             
-            const left = gems[l];
-            hit.set(left,  hit.get(left) - 1);
-            if(!hit.get(left))
-                hit.delete(left);
-            
-            l++;
-        }else{
-            r++;
-            const right = gems[r];
-            hit.set(right, hit.get(right) ? hit.get(right)+1 : 1);
-        }
+        } else {
+            right++;
+            map.set(gems[right], (map.get(gems[right]) || 0) + 1);
+        }     
     }
     
     return answer;
