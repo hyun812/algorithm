8e9a3c208bcd5a43e9a6ba1ff387ed45fffe20f3
SeungHyun <dltmdgus4802@gmail.com>
2025-08-21 11:40:37 +0900

    [level 3] Title: 부대복귀, Time: 6237.72 ms, Memory: 186 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/부대복귀.js
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.js
@@ -1,41 +1,33 @@
 function solution(n, roads, sources, destination) {
     var answer = [];
     
-    const road = new Array(n+1).fill().map(_ => []);
-    roads.forEach((r) => {
-        road[r[0]].push(r[1]);
-        road[r[1]].push(r[0]);
-    })
+    const graph = Array.from({ length: n + 1 }, () => []);
+    for (const road of roads) {
+        const [from, to] = road;
+        graph[from].push(to);
+        graph[to].push(from);
+    }
     
-    const visited = new Array(n+1).fill(Infinity);
-    
-    const bfs = (source) => {
-        const queue = [source];
+    const dijkstra = () => {
+        const queue = [[destination, 0]];
+        const dist = Array(n + 1).fill(Infinity);
         
-        visited[source] = 0;
+        dist[destination] = 0;
         
-        while(queue.length){
-            const cur = queue.shift();
+        while (queue.length) {
+            const [cur, cost] = queue.shift();
             
-            for(let i=0; i<road[cur].length; i++){
-                const next = road[cur][i];
-                
-                if(visited[cur] + 1 < visited[next]){
-                    visited[next] = visited[cur] + 1;
-                    queue.push(next);   
+            for (const next of graph[cur]) {
+                if (dist[next] > dist[cur] + 1) {
+                    dist[next] = dist[cur] + 1;
+                    queue.push([next, dist[next] + 1]);
                 }
             }
         }
+        return dist;
     }
     
-    bfs(destination); // 목적지로부터 각각의 최단거리 구하기
+    const results = dijkstra();
     
-    sources.forEach((source) => {
-        const time = visited[source] === Infinity ? -1 : visited[source]       
-        answer.push(time);
-    })
-    
-    
-          
-    return answer;
+    return sources.map(v => results[v] === Infinity ? -1 : results[v]);
 }
