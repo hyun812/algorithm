efa8445172ab02412ac2b212fe9f21e3a713d40e
SeungHyun <dltmdgus4802@gmail.com>
2025-08-22 15:37:58 +0900

    [level 3] Title: 스티커 모으기(2), Time: 9.17 ms, Memory: 40.3 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.js
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.js
@@ -1,21 +1,23 @@
 function solution(sticker) {
-    const len = sticker.length;
+    const n = sticker.length;
     
-    const dp1 = new Array(len).fill(0); // 첫 번째 스티커를 뗄 경우
-    const dp2 = new Array(len).fill(0); // 첫 번째 스티커를 떼지 않을 경우
-  
+    if(n === 1) return sticker[0];
+
+    const dp1 = Array(n).fill(0); // 첫 스티커 뗀 경우
+    const dp2 = Array(n).fill(0); // 첫 스티커 안뗀 경우
+    
     dp1[0] = sticker[0];
     dp1[1] = sticker[0];
-    dp2[1] = sticker[1];
-    
-    for(let i=2; i<len; i++){
-        if(i === len -1){ // 마지막 스티커라면 dp1은 사용하지 못함
-            dp1[i] = dp1[i-1];
-        }else{
-            dp1[i] = Math.max(dp1[i-1], dp1[i-2] + sticker[i]);    
-        }
-        dp2[i] = Math.max(dp2[i-1], dp2[i-2] + sticker[i]);
+    for (let i = 2; i < n - 1; i ++) {
+        dp1[i] = Math.max(dp1[i - 1], dp1[i - 2] + sticker[i]);
     }
     
-    return Math.max(dp1[len-1], dp2[len-1]);
+    dp2[1] = sticker[1];
+    for (let i = 2; i < n; i ++) {
+        dp2[i] = Math.max(dp2[i - 1], dp2[i - 2] + sticker[i]);
+    }
+    
+    const ans1 = Math.max(...dp1);
+    const ans2 = Math.max(...dp2);
+    return Math.max(ans1, ans2);
 }
