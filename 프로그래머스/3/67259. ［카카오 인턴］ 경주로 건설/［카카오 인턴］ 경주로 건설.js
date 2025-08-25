179ddcffb6deb0c8e34391021fe01cd83297b244
SeungHyun <dltmdgus4802@gmail.com>
2025-08-25 17:20:26 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 50.57 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.js
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.js
@@ -1,38 +1,38 @@
 function solution(board) {
-    const len = board.length;
+    let answer = Infinity;
+    const n = board.length;
+    
     const dy = [0, 1, 0, -1];
     const dx = [1, 0, -1, 0];
-    
-    const queue = [];
-    const dp = Array(len).fill().map(() => Array(len).fill().map(() => Array(4).fill(Infinity)));
-    
-    queue.push([0, 0, 0, 0]);
-    queue.push([0, 0, 0, 1]);
-    
-    const dfs = () => {
+    const bfs = () => {
+        const queue = [[0, 0, 0, 0], [0, 0, 1, 0]];
+        const dp = Array.from({ length: n }, () => Array.from({ length: n }, () => Array(4).fill(Infinity)));
         
-        while(queue.length){
-            const [ y, x, cost, dir ] = queue.shift();
+        while (queue.length) {
+            const [y, x, dir, cost] = queue.shift();
             
-            for(let i=0; i<4; i++){
-                const ny = dy[i] + y;
-                const nx = dx[i] + x;
+            if (y === n - 1 && x === n - 1) {
+                answer = Math.max(answer, cost);
+                continue;
+            }
+            
+            for (let i = 0; i < 4; i++) {
+                const ny = y + dy[i];
+                const nx = x + dx[i];
                 
-                if(ny<0 || nx<0 || ny>=len || nx>= len) continue;
-                if(board[ny][nx]) continue;
+                if (ny < 0 || nx < 0 || ny >= n || nx >= n) continue;
+                if (board[ny][nx] === 1) continue;
                 
-                let roadCost = dir === i ? cost+100 : cost+600;
-                
-                // 비용이 더 크다면
-                if(dp[ny][nx][i] > roadCost) { 
-                    queue.push([ny, nx, roadCost, i]);
-                    dp[ny][nx][i] = roadCost;    
+                const newCost = cost + (dir === i ? 100 : 600);
+                if (newCost < dp[ny][nx][i]) {
+                    dp[ny][nx][i] = newCost;
+                    queue.push([ny, nx, i, newCost]);
                 }
             }
         }
+        return Math.min(...dp[n - 1][n - 1]);
     }
     
-    dfs();
     
-    return Math.min(...dp[len-1][len-1]);
+    return bfs();
 }
