53e41c0de5e8c27199343e43d2f2fdaab258fc87
SeungHyun <dltmdgus4802@gmail.com>
2025-08-25 14:31:57 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 28.31 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -1,28 +1,21 @@
-/*
-    bridge_length: 다리에 올라갈 수 있는 트럭 수
-    weight: 다리가 견딜 수 있는 무게
-    truck_weights: 트럭 별 무게 배열
-    
-    트럭은 정해진 순서대로 건넌다
-*/
 function solution(bridge_length, weight, truck_weights) {
-    let answer = 0;
+    let time = 0;
+    const bridge = Array(bridge_length).fill(0);
+    let cur = 0;
     
-    const bridge = new Array(bridge_length).fill(0);
-    
-    while(bridge.length){
-        bridge.shift();
-        
-        if(truck_weights.length){
-            const bridge_weight = bridge.reduce((acc, cur) => acc + cur, 0);
-            if(bridge_weight + truck_weights[0] <= weight){
-                bridge.push(truck_weights.shift());
-            }else{
+    while (truck_weights.length > 0 || cur > 0) {
+        time++;
+        cur -= bridge.shift();
+        if (truck_weights.length > 0) {
+            if (cur + truck_weights[0] <= weight) {
+                const pop = truck_weights.shift();
+                cur += pop;
+                bridge.push(pop);                
+            } else {
                 bridge.push(0);
             }
         }
-        answer++;
     }
-
-    return answer;
+    
+    return time;
 }
