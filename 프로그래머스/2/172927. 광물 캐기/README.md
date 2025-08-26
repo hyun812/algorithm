1c42c835f203eac801928b36628cb4df44f926af
SeungHyun <dltmdgus4802@gmail.com>
2025-08-26 21:58:19 +0900

    [level 2] Title: 광물 캐기, Time: 0.40 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/README.md
+++ 프로그래머스/2/172927. 광물 캐기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 광물 캐기 - 172927 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/172927) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/172927#qna) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.47 ms
+메모리: 33.5 MB, 시간: 0.40 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 26일 09:45:00
+2025년 08월 26일 21:58:13
 
 ### 문제 설명
 
--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.js
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.js
@@ -1,43 +1,45 @@
-/*
-    광물 5개를 캔 후에는 더 이상 사용할 수 없음
-    1번 시작하면 사용 할 수 없을 때까지 사용
-    광물은 주어진 순서대로 캐야함
-    모든 광물을 캐거나 더 사용할 곡괭이 없을때까지
-    최소한의 피로도
-*/
+// 광물 5개까지 캘 수 있음
+// 광물이 없거나, 곡괭이가 없을 때 까지
 function solution(picks, minerals) {
     let answer = 0;
-        
-    minerals = minerals.splice(0, picks.reduce((a,b) => a+5*b, 0));
-    const fatigue = [{'diamond' : 1 , 'iron' : 1 , 'stone' : 1},
-                     {'diamond' : 5 , 'iron' : 1 , 'stone' : 1},
-                     {'diamond' : 25 , 'iron' : 5 , 'stone' : 1}]
+    const n = minerals.length;
+    const maxLen = (picks[0] + picks[1] + picks[2]) * 5;
     
-    const arr = [];
-    for(let i=0; i<minerals.length; i+=5){
-        arr.push(minerals.slice(i, i+5));
-    }
-        
+    minerals.splice(maxLen);
+    const priority = [];
     
-    arr.sort((a,b) => {
-        const aDiaCnt = a.filter(el => el === 'diamond').length;
-        const bDiaCnt = b.filter(el => el === 'diamond').length;
+    for (let i = 0; i < minerals.length; i += 5) {
+        const temp = [0, 0, 0];
         
-        if(aDiaCnt === bDiaCnt) {
-            const aIronCnt = a.filter(el => el === 'iron').length;
-            const bIronCnt = b.filter(el => el === 'iron').length;
-          return bIronCnt - aIronCnt
+        for (let j = i; j < i + 5; j++) {
+            if (minerals[j] === 'diamond') temp[0]++;
+            else if (minerals[j] === 'iron') temp[1]++;
+            else if (minerals[j] === 'stone') temp[2]++;            
         }
-        return bDiaCnt-aDiaCnt
-    })
+        priority.push(temp);
+    }
     
-    let i= picks[0] ? 0 : picks[1] ? 1 : 2;
+    priority.sort((a, b) => {
+        if (a[0] !== b[0]) {
+            return b[0] - a[0] 
+        } else if (a[1] !== b[1]) {
+            return b[1] - a[1];
+        } else if (a[2] !== b[2]) {
+            return b[2] - a[2];
+        }
+    });
     
-    for (const mine of arr){
-        answer += mine.reduce((a,c) => a+fatigue[i][c],0)
-        if (--picks[i]<=0) i++
-        if (picks.every(el => !el)) return answer
-        
+    for (const [d, i, s] of priority) {
+        if (picks[0]) {
+            answer += (d + i + s);
+            picks[0]--;
+        } else if (picks[1]) {
+            answer += (d * 5) + i + s;
+            picks[1]--;
+        } else if (picks[2]) {
+            answer += (d * 25) + (i * 5) + s;
+            picks[2]--;
+        }
     }
     return answer;
 }
