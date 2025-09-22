dc6c778c0784586f88fc0e543a741a8bfd03e083
SeungHyun <dltmdgus4802@gmail.com>
2025-09-22 23:50:32 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.40 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.js
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.js
@@ -1,52 +1,45 @@
 function solution(places) {
-    const dy = [-1,1,0,0];
-    const dx = [0,0,-1,1];
-    
-    const check = (arr, y, x) => {
-        
-        for(let i=0; i<4; i++){
-            const ny = y+dy[i];
-            const nx = x+dx[i];
-
-            if(ny<0 || nx<0 || ny>=5 || nx>=5) continue;
-            if(arr[ny][nx] === 'P') return false;
-            
-            if(arr[ny][nx] === "O"){
-                
-                for(let j=0; j<4; j++){
-                    const nny = ny+dy[j];
-                    const nnx = nx+dx[j];
-                    
-                    if(nny<0 || nnx<0 || nny>=5 || nnx>=5) continue;
-                    if (nnx === x && nny === y) continue;
-                    if (arr[nny][nnx] === "P") return false
-                
-                }
-            }
-        }
-        return true;
-    }
     const answer = [];
-    for(let i=0; i<5; i++){
-            
-        const arr = places[i];
-        let isSafe = 1;
-        for(let j=0; j<arr.length; j++){
-            for(let k=0; k<arr[0].length; k++){
-                if(arr[j][k] !== 'P') continue;
-                
-                if(!check(arr, j, k)){
-                    isSafe = 0;
-                    break;
-                }
-            }
-            
-            if(isSafe === 0){
-                break;
+    
+    const dy = [0, 0, -1, 1];
+    const dx = [-1, 1, 0, 0];
+    const isPosibblePlace = (place) => {
+        const queue = [];
+        for (let i = 0; i < 5; i++) {
+            for (let j = 0; j < 5; j++) {
+                if (place[i][j] !== 'P') continue;
+                queue.push([i, j]);
             }
         }
-        answer.push(isSafe);
-    }
         
+        while (queue.length) {
+            const [y, x] = queue.shift();
+            
+            for (let i = 0; i < 4; i++) {
+                const ny = y + dy[i];
+                const nx = x + dx[i];
+                
+                if (ny < 0 || nx < 0 || ny >= 5 || nx >= 5) continue;
+                if (place[ny][nx] === 'P') return 0;
+                if (place[ny][nx] === 'X') continue;
+                
+                for (let j = 0; j < 4; j++) {
+                    const nny = ny + dy[j];
+                    const nnx = nx + dx[j];
+
+                    if (nny < 0 || nnx < 0 || nny >= 5 || nnx >= 5) continue;
+                    if (nny === y && nnx === x) continue;
+                    if (place[nny][nnx] === 'P') return 0;
+                }
+            }
+        }
+        
+        return 1;
+    }
+    
+    for (const place of places) {
+        answer.push(isPosibblePlace(place))
+    }
+    
     return answer;
 }
