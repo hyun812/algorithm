290cba08d8f5eb1a1e1eb8c13ed77a34ac5b50f9
SeungHyun <dltmdgus4802@gmail.com>
2025-09-24 15:27:26 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 84.41 ms, Memory: 51.3 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.js
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.js
@@ -1,51 +1,50 @@
 function solution(land) {
-    let answer = 0;
+    const N = land.length;
+    const M = land[0].length;
     
-    const [n, m] = [land.length, land[0].length];
-    const dy = [-1, 1, 0, 0];
-    const dx = [0, 0, -1, 1];
-    let totalCount = new Array(m).fill(0);
+    const dy = [0, 0, -1, 1];
+    const dx = [-1, 1, 0, 0];
+    const visited = Array.from({ length: N }, () => Array(M).fill(0));
     
+    const answer = Array(M).fill(0);
     const bfs = (startY, startX) => {
-        
-        let indexSet = new Set();
-        let count = 0;
+        let results = 1;
+        const set = new Set();
         const queue = [[startY, startX]];
-        indexSet.add(startX);
+        visited[startY][startX] = 1;
+        set.add(startX);
         
-        while(queue.length){
+        while (queue.length) {
             const [y, x] = queue.shift();
             
-            if(land[y][x] === 0) continue;
-            count++;
-            land[y][x] = 0;
-            
-            for(let i=0; i<4; i++){
-                const ny = dy[i] + y;
-                const nx = dx[i] + x;
+            for (let i = 0; i < 4; i++) {
+                const ny = y + dy[i];                
+                const nx = x + dx[i];
                 
-                if(ny<0 || nx<0 || ny>=n || nx>= m) continue;
-                if(land[ny][nx] === 0) continue;
+                if (ny < 0 || nx < 0 || ny >= N || nx >= M) continue;
+                if (visited[ny][nx]) continue;
+                if (land[ny][nx] === 0) continue;
                 
                 queue.push([ny, nx]);
-                indexSet.add(nx);
+                set.add(nx);
+                visited[ny][nx] = 1;
+                results++;
             }
         }
         
-        return [count, Array.from(indexSet)];
-    }
-    
-    for(let i=0; i<n; i++){
-        for(let j=0; j<m; j++){
-            if(land[i][j] === 1){
-                const [count, indexArr] = bfs(i, j);
-
-                for(let index of indexArr){
-                    totalCount[index] += count;
-                }
-            }
+        for (const v of set) {
+            answer[v] += results;
         }
     }
     
-    return Math.max(...totalCount);
+    
+    for (let i = 0; i < N; i++) {
+        for (let j = 0; j < M; j++) {
+            if (land[i][j] === 0) continue;
+            if (visited[i][j]) continue;
+            bfs(i, j);
+        }
+    }
+    
+    return Math.max(...answer);
 }
