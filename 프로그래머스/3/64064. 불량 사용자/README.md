d5fb82bbaefd06177d30cf720588d8f0ef708c22
SeungHyun <dltmdgus4802@gmail.com>
2025-10-15 16:42:13 +0900

    [level 3] Title: 불량 사용자, Time: 35.24 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/README.md
+++ 프로그래머스/3/64064. 불량 사용자/README.md
@@ -1,10 +1,10 @@
 # [level 3] 불량 사용자 - 64064 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64064) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64064?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.65 ms
+메모리: 38.1 MB, 시간: 35.24 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 23일 09:18:53
+2025년 10월 15일 16:42:09
 
 ### 문제 설명
 
--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.js
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.js
@@ -1,68 +1,38 @@
 function solution(user_id, banned_id) {
     let answer = 0;
-    let visit = [];
-    const set = new Set();
     
-    const check = (a, b) => {
-        for(let i=0; i<a.length; i++){
-            if(a[i] === '*') continue;
-            if(a[i] !== b[i]) return false;
+    const isPossible = (user, target) => {
+        if (user.length !== target.length) return false;
+
+        for (let i = 0; i < user.length; i++) {
+            if (target[i] === '*') continue;
+            if (user[i] !== target[i]) return false;
         }
         return true;
     }
     
-    const dfs = (idx, count, str) => {
-        if(count === banned_id.length) {
+    const set = new Set();
+    const visited = Array(user_id.length).fill(0);
+    const dfs = (index) => {
+        if (index === banned_id.length) {
+            const key = visited.join('');
+            if (!set.has(key)) answer++;
+            set.add(key);
             return;
         }
         
-        for(let i=idx; i<banned_id.length; i++){
-            for(let j=0; j<user_id.length; j++){
-                if(visit[j]) continue;                
-                if(banned_id[i].length !== user_id[j].length) continue;
-                if(!check(banned_id[i],user_id[j])) continue;
-                
-                visit[j]=1;
-                dfs(i+1, count+1, str+" "+user_id[j]);
-                visit[j]=0;
-            }
-        }
-
-    }
-    
-    dfs(0,0,"");
-    
-    const makeList=(idx_i,count,str)=>{
-        if(count===banned_id.length){
-            let arr=str.split(" ");
-            arr.shift();
-            arr.sort();
-            let newstr=arr.join("");
-            set.add(newstr);
-        } 
-        if(idx_i>=user_id.length) return;
-        
-        for(let i=idx_i;i<banned_id.length;i++){
-            for(let j=0;j<user_id.length;j++){
-                /**
-                	중복체크
-                **/
-                if(visit[j]) continue;
-                /**
-                    조건일치하지 않는 경우 무시
-                **/
-                if(banned_id[i].length!==user_id[j].length) continue;
-                if(!check(banned_id[i],user_id[j])) continue;
-                /**
-                	백트래킹
-                **/
-                visit[j]=1;
-                makeList(i+1,count+1,str+" "+user_id[j]);
-                visit[j]=0;
+        const target = banned_id[index];
+        for (let i = 0; i < user_id.length; i++) {
+            if (visited[i]) continue;
+            if (isPossible(user_id[i], target)) {
+                visited[i] = 1;
+                dfs(index + 1);
+                visited[i] = 0;
             }
         }
     }
     
-    makeList(0,0,"");
-    return set.size;
+    dfs(0);
+    
+    return answer;
 }
