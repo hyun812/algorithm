49b84aafaf665d96b35fa6e0779d43c26da68c56
SeungHyun <dltmdgus4802@gmail.com>
2025-10-17 15:56:10 +0900

    [level 3] Title: 가장 먼 노드, Time: 42.45 ms, Memory: 55.5 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
@@ -1,35 +1,38 @@
-const bfs = (n, arr) => {
-    const queue = [1];
-    let visited = new Array(n+1).fill(0);
+function solution(n, edge) {
+    let answer = 0;
+    let max = 0;
     
-    visited[1] = 1;
+    const graph = Array.from({ length: n + 1 }, () => []);
+    for (const [from, to] of edge) {
+        graph[from].push(to)
+        graph[to].push(from)
+    }
     
-    while(queue.length){
-        const cur = queue.shift();
-
-        for(const next of arr[cur]){
-            if(visited[next]) continue;
-
-            visited[next] = visited[cur] + 1;
-            queue.push(next);
+    const bfs = () => {
+        const queue = [[1, 0]];
+        const visited = Array(n + 1).fill(0);
+        
+        visited[1] = 1;
+        
+        while (queue.length) {
+            const [cur, len] = queue.shift();
+            
+            if (len > max) {
+                max = len;
+                answer = 1;
+            } else if (len === max) {
+                answer++;   
+            }
+            
+            for (const next of graph[cur]) {
+                if (visited[next]) continue;
+                visited[next] = 1;
+                queue.push([next, len + 1]);
+            }
         }
     }
     
-    return visited;
-}
-
-function solution(n, edge) {
-    let answer = 0;
+    bfs();
     
-    const arr = new Array(n+1).fill().map(_ => []);
-    
-    for(const e of edge){
-        arr[e[0]].push(e[1]);
-        arr[e[1]].push(e[0]);
-    }    
-    
-    const visited = bfs(n, arr);
-    const max = Math.max(...visited);
-    
-    return visited.filter(el => el === max).length;
+    return answer;
 }
