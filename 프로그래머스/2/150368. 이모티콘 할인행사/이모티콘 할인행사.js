f21a5d3599820cffdc895a0987d7d09fc348b1be
SeungHyun <dltmdgus4802@gmail.com>
2025-10-27 09:35:23 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 63.23 ms, Memory: 39.2 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.js
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.js
@@ -1,50 +1,45 @@
 function solution(users, emoticons) {
-    var answer = [];
+    let answer = [0, 0];
     
-    const permutations = getPermutations([10, 20, 30, 40], emoticons.length);
-
-    permutations.forEach((permutation) => {
-        let count = 0;
-        const costs = Array(users.length).fill(0);
+    const calc = (discount) => {
+        let [count, sum] = [0, 0];
         
-        permutation.forEach((perm, idx) => {
-            for(let i=0; i<users.length; i++){
-                if(users[i][0] <= perm){
-                    costs[i] += emoticons[idx] * (100-perm) / 100;
-                }
+        users.forEach(([rate, price]) => {
+            let sumPrice = 0;
+            for (let i = 0; i < emoticons.length; i++) {
+                if (rate > discount[i]) continue;
+                sumPrice += (emoticons[i] / 100 * (100 - discount[i]));
             }
+            
+            if (sumPrice >= price) {
+                count++
+            } else sum += sumPrice;
         })
-        let price = 0;
-        costs.forEach((cost, idx) => {
-            if(cost >= users[idx][1]){ 
-                count++;
-            }else{
-                price += cost;
+
+        return [count, sum];
+    }
+    
+    const dfs = (index, discount) => {
+        if (index === emoticons.length) {
+            const [count, sum] = calc(discount);
+            
+            if (answer[0] < count) {
+                answer[0] = count;
+                answer[1] = sum;
+            } else if (answer[0] === count) {
+                answer[1] = Math.max(answer[1], sum);
             }
-        })
+
+            return;
+        }
         
-        answer.push([count, price]);
-    })
+        for (const d of [10, 20, 30, 40]) {
+            dfs(index + 1, [...discount, d]);
+        }
+    }
     
-    answer.sort((a,b) => {
-        return a[0] !== b[0] ? b[0] - a[0] : b[1] - a[1];
-    })
-    
-    return answer[0];
-}
-
-
-function getPermutations(arr, selectNumber) { // 중복 순열
-    const result = [];
-
-    if (selectNumber === 1) return arr.map((el) => [el]);
-
-    arr.forEach((fixed, index, origin) => {
-        const permutations = getPermutations(origin, selectNumber - 1); 
-        const attached = permutations.map((el) => [fixed, ...el]); 
-        
-        result.push(...attached); 
-    });
-    
-  return result;
+    dfs(0, []);
+          
+          
+    return answer;
 }
