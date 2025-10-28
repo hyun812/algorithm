c4a9d92a59aebc6064c5b6c293e2b8b8afadf3df
SeungHyun <dltmdgus4802@gmail.com>
2025-10-28 16:04:26 +0900

    [level 2] Title: 괄호 변환, Time: 0.68 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/2/60058. 괄호 변환/괄호 변환.js
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.js
@@ -1,35 +1,48 @@
-function solution(p) {
-    // 빈문자열인 경우 반환
-    if(!p) return "";
+// 개수가 같으면 균형잡힌 괄호 문자열
+
+function solution(p) {    
+    const checkCorrect = (words) => {
+        const temp = [];
+        for (const char of words) {
+            if (char === '(') {
+                temp.push(char);
+            } else {
+                if (temp[temp.length - 1] === '(') {
+                    temp.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        if (temp.length) return false;
+        return true;
+    }
     
-    let [u, v] = ["", ""];
-    let [lcnt, rcnt] = [0, 0];
-    
-    // 더 이상 분리할 수 없는 균형잡힌 문자열 u 생성
-    for(let i=0; i<p.length; i++){
-        p[i] === '(' ? ++lcnt : ++rcnt;
+    const conversionWords = (p) => {
+        if (p === '') return p;
+        if (checkCorrect(p)) return p;
         
-        u += p[i];
+        let open = 0;
+        let close = 0;
+        for (const char of p) {
+            if (open !== 0 && open === close) break;
+            char === '(' ? open++ : close++;
+        }
+
+        let u = p.slice(0, open + close);
+        let v = p.slice(open + close);
         
-        if(lcnt === rcnt){
-            v = p.slice(i+1, p.length);
-            break;
+        if (checkCorrect(u)) {
+            return u + conversionWords(v)
+        } else {
+            let newV = '(' + conversionWords(v) + ')';
+            
+            for (let i = 1; i < u.length - 1; i++) {
+                newV += u[i] === '(' ? ')' : '(';
+            }
+            return newV;
         }
     }
     
-    // u가 올바른 괄호 문자열이라면
-    if(u[u.length -1] === ')') return u+solution(v);
-    else { // u가 올바른 문자열이 아니라면
-        
-        // 첫번째와 마지막 문자 제거
-        u = u.slice(1, u.length -1).split("");
-        
-        // 나머지 문자열 괄호 방향 뒤집기
-        for(let i=0; i<u.length; i++){
-            u[i] = u[i] === ')' ? '(' : ')';
-        }
-        
-        // ( + v에 대해 수행한 문자열 + ) + 뒤에 붙이기
-        return "(" + solution(v) + ")" + u.join("");
-    }
+    return conversionWords(p);
 }
