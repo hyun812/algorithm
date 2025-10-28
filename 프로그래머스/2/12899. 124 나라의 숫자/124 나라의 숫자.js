5aad87465566969eae4ef0d28ff59eb4a7fc14fe
SeungHyun <dltmdgus4802@gmail.com>
2025-10-28 15:34:37 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.js
+++ 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.js
@@ -1,13 +1,12 @@
 function solution(n) {
     let answer = '';
-    
-    // 3으로 나눈 나머지를 이용 순서 4,1,2
-    const mod = ["4","1","2"];
-    
-    while(n > 0){    
-        answer = mod[n%3] + answer;
-        //3과 정확히 떨어지면 몫이 1이 생기기 때문에 -1을 해준후 계산한다.
-        n = parseInt((n-1)/3);
+    const nums = [4, 1, 2];
+
+    while (n) {
+        const result = n % 3;
+        n = Math.floor((n - 1) / 3);
+        
+        answer = nums[result] + answer;
     }
     
     return answer;
