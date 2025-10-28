5cc1250567b9cc1c33798e4e13f0b47fde6fd0aa
SeungHyun <dltmdgus4802@gmail.com>
2025-10-28 14:59:39 +0900

    [level 2] Title: N-Queen, Time: 464.43 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.js
+++ 프로그래머스/2/12952. N－Queen/N－Queen.js
@@ -1,31 +1,30 @@
 function solution(n) {
     let answer = 0;
-    const board = Array(n).fill(0);
+    const queens = [];
     
-    const check = (y, x, board) => {
-        for (let i = 0 ; i < y; i++) {
-            if (board[i] === x) return false;
-            if (Math.abs(y - i) === Math.abs(x - board[i])) return false;
+    const check = (checkY, checkX) => {
+        for (const [y, x] of queens) {
+            if (checkY === y || checkX === x) return false;
+            if (Math.abs(checkY - y) === Math.abs(checkX - x)) return false;
         }
-        
         return true
     }
     
-    const dfs = (y, board) => {
-        const tmpBoard = [...board];
+    const dfs = (y) => {
         if (y === n) {
             answer++;
             return;
         }
         
         for (let x = 0; x < n; x++) {
-            if (!check(y, x, tmpBoard)) continue;
-            tmpBoard[y] = x;
-            dfs(y + 1, tmpBoard);
+            if (!check(y, x)) continue;
+            queens.push([y, x]);
+            dfs(y + 1);
+            queens.pop();
         }   
     }
     
-    dfs(0, board);
+    dfs(0);
     
     return answer;
 }
