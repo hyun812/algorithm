48c29045fdc08c9efa9baab9bf26e6c519e6e7de
SeungHyun <dltmdgus4802@gmail.com>
2025-10-30 16:25:44 +0900

    [level 3] Title: 거스름돈, Time: 37.40 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/3/12907. 거스름돈/거스름돈.js
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.js
@@ -1,15 +1,13 @@
-// n: 거슬러 줘야하는 금액
-// money: 현재 보유하고 있는 돈의 종류
 function solution(n, money) {
-    let answer = 0;
+    const MOD = 1000000007;
+    const dp = new Array(n + 1).fill(0);
     
-    const dp = new Array(n+1).fill(0);
     dp[0] = 1;
     
-    for(const value of money){
-        for(let i=value; i<=n; i++){
-            dp[i] = (dp[i] + dp[i-value]) % 1000000007;
-        }    
+    for (let m of money) {
+        for (let i = m; i <= n; i++) {
+            dp[i] = (dp[i] + dp[i - m]) % MOD;
+        }
     }
     
     return dp[n];
