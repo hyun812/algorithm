dd497c2c2d95b1ac0359d8f266749aa163b6b9db
SeungHyun <dltmdgus4802@gmail.com>
2025-11-11 21:45:26 +0900

    [level 1] Title: 체육복, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -1,14 +1,14 @@
 function solution(n, lost, reserve) {
-  let lost_student = lost.filter(v => !reserve.includes(v)).sort();
-  let reserve_student = reserve.filter(v => !lost.includes(v)).sort();
-
-  for (let i = 0; i < reserve_student.length; i++) {
-    if (lost_student.includes(reserve_student[i] - 1)) {
-      lost_student = lost_student.filter(v => v !== reserve_student[i] - 1);
-    } else if (lost_student.includes(reserve_student[i] + 1)) {
-      lost_student = lost_student.filter(v => v !== reserve_student[i] + 1);
+    let lost_student = lost.filter(v => !reserve.includes(v)).sort((a, b) => a - b);;
+    let reserve_student = reserve.filter(v => !lost.includes(v)).sort((a, b) => a - b);;
+    
+    for (const num of reserve_student) {
+        if (lost_student.includes(num - 1)) {
+            lost_student = lost_student.filter((v) => v !== num - 1);
+        } else if (lost_student.includes(num + 1)) {
+            lost_student = lost_student.filter((v) => v !== num + 1);
+        }
     }
-  }
-
-  return n - lost_student.length;
+    
+    return n - lost_student.length;
 }
