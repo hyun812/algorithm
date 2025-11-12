5481e166ffa3a2cf6a5402e6cd41597db7cdcce9
SeungHyun <dltmdgus4802@gmail.com>
2025-11-12 17:51:19 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 37.83 ms, Memory: 55.8 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/README.md
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/README.md
@@ -1,10 +1,10 @@
 # [level 2] [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지 - 340212 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340212#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340212?language=javascript) 
 
 ### 성능 요약
 
-메모리: 55.8 MB, 시간: 60.35 ms
+메모리: 55.8 MB, 시간: 37.83 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 10일 16:51:14
+2025년 11월 12일 17:51:15
 
 ### 문제 설명
 
--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.js
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.js
@@ -1,36 +1,45 @@
-function solution(diffs, times, limit) {
+/*
+    diffs: 난이도
+    time_cur: 현재 퍼즐 소요시간
+    time_prev: 이전 퍼즐 소요시간
+    level: 숙련도
     
-    const check_time = (level) => {
-        let prev_time = 0;
-        let all_time = 0;
-        for(let i=0; i<diffs.length; i++) {
-            let diff = diffs[i];
-            let cur_time = times[i];
-        
-            if(diff > level){
-                all_time += ((prev_time + cur_time) * (diff - level));
+    diff - level 만큼 퍼즐을 틀림
+    퍼즐을 틀릴 때마다 time_cur + time_prev 만큼의 시간 소요 + time_cur
+    제한 시간내에 퍼즐을 모두 해결하기위한 숙련도의 최솟값
+    이분탐색을 숙련도로 계산해서 문제 해결
+*/
+function solution(diffs, times, limit) {
+    const n = diffs.length;
+    
+    const getTotalTime = (level) => {
+        let total_time = 0;
+        for (let i = 0; i < n; i++) {
+            if (diffs[i] <= level) {
+                total_time += times[i];
+            } else {
+                const diff = diffs[i] - level;
+                total_time += ((times[i - 1] || 0) + times[i]) * diff + times[i]
             }
-            
-            all_time += cur_time;
-            prev_time = cur_time;
         }
-        
-        return all_time <= limit
+        return total_time;
     }
     
-    
     let left = 1;
-    let right = 100000;
-    let answer = right;
+    let right = 100001;
+    let answer = right; 
     
-    while(left <= right) {
+    while (left <= right) {
         const mid = Math.floor((left + right) / 2);
         
-        if(check_time(mid)) {
-            right = mid - 1;
-            answer = Math.min(answer, mid);
-        }else {
+        // 총 걸리는 시간
+        const total_time = getTotalTime(mid);
+
+        if (total_time > limit) {
             left = mid + 1;
+        } else {
+            answer = Math.min(mid, answer);
+            right = mid - 1;
         }
     }
     
