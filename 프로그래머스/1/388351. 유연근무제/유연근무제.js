054946d7e7fb217ebdc38288954f853e88dd0089
SeungHyun <dltmdgus4802@gmail.com>
2025-11-15 19:52:39 +0900

    [level 1] Title: 유연근무제, Time: 2.25 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/1/388351. 유연근무제/유연근무제.js
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.js
@@ -2,32 +2,25 @@ function solution(schedules, timelogs, startday) {
     const n = schedules.length;
     
     const convertTime = (time) => {
-        const s = String(time).padStart(4, '0');
-        const h = Number(s.slice(0, 2));
-        const m = Number(s.slice(2, 4));
-        return h * 60 + m;
+        return Math.floor(time / 100) * 60 + time % 100;
     }
     
     schedules = schedules.map(time => convertTime(time));
     timelogs = timelogs.map(log => log.map(time => convertTime(time)));
     
     let answer = 0;
-    let day = startday - 1;
+    
     for (let i = 0; i < n; i++) {
-        const timelog = timelogs[i];
         let flag = true;
-        for (const time of timelog) {
-            day++;
-            if (day == 8) day = 1;
-            if (day === 6 || day === 7) continue;
-            if (schedules[i] + 10 < time) {
+        for (let j = 0; j < 7; j++) {
+            const today = (startday + j) % 7;
+            if (today === 6 || today === 0) continue;
+            if (schedules[i] + 10 < timelogs[i][j]) {
                 flag = false;
                 break;
             }
-            
+
         }
-        
-        day = startday - 1;
         if (flag) answer++;
     }
     
