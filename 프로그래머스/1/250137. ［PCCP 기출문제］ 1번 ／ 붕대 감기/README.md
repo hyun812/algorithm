d0e6de3e2faf33307511de370230fd690d0f15f5
SeungHyun <dltmdgus4802@gmail.com>
2025-11-17 18:51:05 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/README.md
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/README.md
@@ -1,10 +1,10 @@
 # [level 1] [PCCP 기출문제] 1번 / 붕대 감기 - 250137 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/250137#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/250137) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.31 ms
+메모리: 33.5 MB, 시간: 0.21 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 18일 20:58:09
+2025년 11월 17일 18:50:55
 
 ### 문제 설명
 
--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
@@ -1,30 +1,31 @@
-function solution(bandage, health, attacks) {
-    var answer = 0;
+/**
+    t초 동안 1초마다 x 체력 회복
+    t초 연속 붕대 성공 시 추가 y
     
-    const [time, recoverySecond, addRecovery] = bandage;
-    let max = health;
-    let curTime = 0;
-    let index = 0;
-    let count = 0;
-    while(index <= attacks.length-1){
-        if(health <= 0) break;
-        curTime++;
+    bandage: [t, x, y]
+    health: 최대 체력
+    attacks: [공격 시간, 피해량]
+    
+    return: 남은 체력
+**/
+function solution(bandage, health, attacks) {
+    const [t, x, y] = bandage;
+    
+    let cur = health;
+    let time = 0;
+    
+    for (const [attackTime, damage] of attacks) {
+        // 체력 회복 계산
+        const diff = attackTime - time - 1;
+        const success = Math.floor(diff / t);
+        const recovery = diff * x + success * y;
+        cur += recovery;
+        if (cur >= health) cur = health;        
         
-        if(curTime === attacks[index][0]) {
-            health -= attacks[index][1];
-            index++;
-            count = 0;
-        }else {
-            health += recoverySecond;
-            count++;
-            
-            if(count === time) {
-                health += addRecovery;
-                count = 0;
-            };
-            if(health > max) health = max;
-        }
+        cur -= damage;
+        if (cur <= 0) return -1;
+        time = attackTime;
     }
     
-    return health <= 0 ? -1 : health;
+    return cur;
 }
