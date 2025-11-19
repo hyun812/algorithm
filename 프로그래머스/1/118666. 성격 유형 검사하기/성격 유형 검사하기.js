c12e137c4271a4b24bbbbbbfde599da8d0598071
SeungHyun <dltmdgus4802@gmail.com>
2025-11-19 14:28:01 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.33 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,34 +1,13 @@
 function solution(survey, choices) {
-    const personality = {'RT': [0, 0], "CF": [0, 0], "JM": [0, 0], "AN": [0, 0]}
+    const personality = { 'R': 0, 'T': 0, 'C': 0, 'F': 0, 'J': 0, 'M': 0, 'A': 0, 'N': 0};
     const n = survey.length;
     
-    for (let i = 0; i < survey.length; i++) {
-        if (choices[i] === 4) continue;
-        
-        if (personality[survey[i]]) {
-            if (choices[i] < 4) {
-                personality[survey[i]][0] += 4 - choices[i];
-            } else {
-                personality[survey[i]][1] += choices[i] - 4;
-            }
-        } else {
-            const reverse = `${survey[i][1]}${survey[i][0]}`;
-            if (choices[i] < 4) {
-                personality[reverse][1] += 4 - choices[i];
-            } else {
-                personality[reverse][0] += choices[i] - 4;
-            }
-        }
+    for (let i = 0; i < n; i++) {
+        const score = 4 - choices[i]; // 양수면 첫번째, 음수면 두번째
+        const type = score > 0 ? survey[i][0] : survey[i][1];
+        personality[type] += Math.abs(score);
     }
     
-    let answer = '';
-    for (const key in personality) {
-        if (personality[key][0] >= personality[key][1]) {
-            answer += key[0];
-        } else {
-            answer += key[1];
-        }
-    }
-    
-    return answer;
+    const { R, T, C, F, J, M, A, N } = personality;
+    return `${R >= T ? 'R' : 'T'}${C >= F ? 'C' : 'F'}${J >= M ? 'J' : 'M'}${A >= N ? 'A' : 'N'}`
 }
